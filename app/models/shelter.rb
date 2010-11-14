776c4f7d2a16ea2be46c4a63d0564881aee71c63
Brian Bokor <brian.bokor@designwaves.com>
2010-11-14 21:16:50 +0000

    updated address to street

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -4,7 +4,7 @@ class Shelter < ActiveRecord::Base
    
   # Validations
   validates_presence_of :name
-  validates_presence_of :address
+  validates_presence_of :street
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zip_code
