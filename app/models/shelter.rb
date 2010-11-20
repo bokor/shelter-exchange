c40e85849f49dc6a3a715c38660f8e2e2f5ecac3
Brian Bokor <brian.bokor@designwaves.com>
2010-11-20 07:15:58 +0000

    added geocoding to shelter

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,4 +1,6 @@
 class Shelter < ActiveRecord::Base
+  acts_as_mappable
+  before_validation :geocode_address
   
   # Associations
   belongs_to :account
@@ -9,11 +11,18 @@ class Shelter < ActiveRecord::Base
    
   # Validations
   validates_presence_of :name
-  validates_presence_of :street
+  validates_presence_of :address
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zip_code
 
   # Scopes
+  
+  private
+    def geocode_address
+      geo=Geokit::Geocoders::MultiGeocoder.geocode ([address, city, state, zip_code].join(" "))
+      errors.add(:address, "Could not Geocode address") if !geo.success
+      self.lat, self.lng = geo.lat,geo.lng if geo.success
+    end
   
 end
