050430d2f6209f733213f615774308d76a218212
Brian Bokor <brian.bokor@designwaves.com>
2011-02-09 06:39:45 +0000

    clean up comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   helper :all
-  before_filter :current_subdomain, :current_shelter #, :get_user #, :set_timezone
+  before_filter :current_subdomain, :current_shelter #, :set_timezone
   layout :current_layout_name
   
   private
@@ -36,18 +36,6 @@ class ApplicationController < ActionController::Base
       # Time.zone = @current_shelter.time_zone
     end
     
-    # Get roles accessible by the current user
-    #----------------------------------------------------
-    # def accessible_roles
-    #   @accessible_roles = Role.accessible_by(current_ability,:read)
-    # end
-    # 
-    # # Make the current user object available to views
-    # #----------------------------------------
-    # def get_user
-    #   @current_user = current_user
-    # end
-    
     # rescue_from CanCan::AccessDenied do |exception|
     #   flash[:error] = exception.message
     #   redirect_to root_url
@@ -72,6 +60,8 @@ class ApplicationController < ActionController::Base
       end
       nil
     end
+    
+
   
     #
     #   OLD WAY
