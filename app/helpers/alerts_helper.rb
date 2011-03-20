82f6fccddd28f667ee87eb1e90fcc1e81c066362
Brian Bokor <brian.bokor@designwaves.com>
2011-03-20 23:09:02 +0000

    cleaning up alert helper code

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -1,8 +1,4 @@
 module AlertsHelper
-  # def subject_url
-  #       subject = controller.controller_name.singularize
-  #       alerts_path(:subject_type => subject, :subject_id => controller.instance_variable_get("@#{subject}").id)
-  #     end
   def show_alertable_link(alert)
 	  if alert.alertable
 	    link = link_to alert.alertable.name, polymorphic_path(alert.alertable)
