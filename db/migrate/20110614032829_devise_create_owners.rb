8a18f765d9de5be5d277b6b868d98d10a7ffb150
Brian Bokor <brian.bokor@designwaves.com>
2011-06-14 05:42:25 +0000

    added devise owners for admin interface

---
+++ db/migrate/20110614032829_devise_create_owners.rb
@@ -0,0 +1,27 @@
+class DeviseCreateOwners < ActiveRecord::Migration
+  def self.up
+    create_table(:owners) do |t|
+      t.database_authenticatable :null => false
+      t.recoverable
+      t.rememberable
+      t.trackable
+      t.string :name
+
+      # t.confirmable
+      # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :email
+      # t.token_authenticatable
+
+
+      t.timestamps
+    end
+
+    add_index :owners, :email,                :unique => true
+    add_index :owners, :reset_password_token, :unique => true
+    # add_index :owners, :confirmation_token,   :unique => true
+    # add_index :owners, :unlock_token,         :unique => true
+  end
+
+  def self.down
+    drop_table :owners
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110611203716) do
+ActiveRecord::Schema.define(:version => 20110614032829) do
 
   create_table "accommodations", :force => true do |t|
     t.integer  "shelter_id"
@@ -37,21 +37,6 @@ ActiveRecord::Schema.define(:version => 20110611203716) do
 
   add_index "accounts", ["subdomain"], :name => "index_accounts_on_subdomain"
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.integer  "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "namespace"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
   create_table "admin_users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
@@ -66,11 +51,29 @@ ActiveRecord::Schema.define(:version => 20110611203716) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "admins", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
+  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
 
   create_table "alerts", :force => true do |t|
     t.string   "title"
@@ -251,6 +254,26 @@ ActiveRecord::Schema.define(:version => 20110611203716) do
   add_index "notes", ["note_category_id"], :name => "index_notes_on_note_category_id"
   add_index "notes", ["shelter_id"], :name => "index_notes_on_shelter_id"
   add_index "notes", ["title"], :name => "index_notes_on_title"
+
+  create_table "owners", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "owners", ["email"], :name => "index_owners_on_email", :unique => true
+  add_index "owners", ["reset_password_token"], :name => "index_owners_on_reset_password_token", :unique => true
 
   create_table "parents", :force => true do |t|
     t.string   "name"
