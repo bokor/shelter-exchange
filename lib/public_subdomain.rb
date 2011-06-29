73c1720e18f8b919079aec3a8c9575a923db67be
designwaves <brian.bokor@designwaves.com>
2011-06-28 22:32:24 -0400

    Updated some issues with the Layouts and changed the will_paginate back to 2.13.5
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- lib/public_subdomain.rb
+++ lib/public_subdomain.rb
@@ -1,5 +1,5 @@
 class PublicSubdomain
   def self.matches?(request)
-    request.subdomain.blank? or request.subdomains.first == "www"
+    request.subdomain.blank? or request.subdomain == "www"
   end
 end
