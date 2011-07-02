611d87ed36d718eaebb10515b8bf21abb19e7026
designwaves <brian.bokor@designwaves.com>
2011-07-02 09:40:42 -0400

    Fixed Comments and Locations Dialogs to hide the form and added links to the top.
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- public/javascripts/app/comments.js
+++ public/javascripts/app/comments.js
@@ -4,14 +4,27 @@
  * ------------------------------------------------------------------------ */
 
 var Comments = {
-	updateCommentableDialog: function(id, commentable, placement_shelter, current_shelter){
-		if(placement_shelter != current_shelter){
-			$("#create_comment").hide();
-		} else {
-			$("#create_comment").show();
-		}
+	initializeDialog: function(){
+		$('.add_comment_link a').bind('click', function(e){
+			e.preventDefault();
+			$('#create_comment').slideToggle(800);
+		});
+		$('.close_link a').bind('click', function(e){
+			e.preventDefault();
+			Tipped.hideAll();
+		});
+	},
+	updateCommentable: function(id, commentable){
 		$("#comment_commentable_id").val(id);
 		$("#comment_commentable_type").val(commentable);
+	},
+	updateDialog: function(placement_shelter, current_shelter){
+		$("#create_comment").hide();
+		if(placement_shelter == current_shelter){
+			$(".add_comment_link").show();
+		} else {
+			$(".add_comment_link").hide();
+		}
 	},
 	cancelForm: function(id){
 		$("#edit_comment_"+id).slideToggle(800,function() {
--- public/javascripts/app/locations.js
+++ public/javascripts/app/locations.js
@@ -3,6 +3,16 @@
  * Copyright (c) 2011 Designwaves, LLC. All rights reserved.
  * ------------------------------------------------------------------------ */
 var Locations = {
+	initializeDialog: function(){
+		$('.add_location_link a').bind('click', function(e){
+			e.preventDefault();
+			$('#create_location').slideToggle(800);
+		});
+		$('.close_link a').bind('click', function(e){
+			e.preventDefault();
+			Tipped.hideAll();
+		});
+	},	
 	findAllLocations: function() {
 		$.get("/locations/find_all.js");
   	},
