15094725bd67a74049261c61a9d12f8bf94459c4
designwaves <brian.bokor@designwaves.com>
2011-07-02 11:46:46 -0400

    Updated map function
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,10 +1,14 @@
 module MapsHelper
   
   def shelter_xml_description(shelter)
-    '<ul>' +
-      '<li>' + shelter.street + '</li>' +
-      '<li>' + shelter.city + ', ' + shelter.state + ' ' + shelter.zip_code + '</li>' +
-    '</ul>' +
-    '<p><img src="http://www.designwaves.com/designwaves-icon.png" />testing this method call</p>'
+    '<ul>' <<
+      '<li>'<< shelter.name << '</li>' <<
+      '<li>' << shelter.street << '</li>' << 
+    	'<li>' << shelter.city << ', ' << shelter.state << ' ' << shelter.zip_code << '</li>' <<
+    	'<li>Phone:' << shelter.phone << '</li>'
+    	'<li>Email:' << link_to(shelter.email, "mailto:#{shelter.email}") << '</li>' <<
+    	'<li>Website:' << link_to(shelter.website, shelter.website) << '</li>' <<
+    '</ul>'
   end
+  
 end
