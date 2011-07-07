6af266dc29973551fdb0eab46560d7bda8b21876
designwaves <brian.bokor@designwaves.com>
2011-07-06 23:31:40 -0400

    cleaning up migrations
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- db/migrate/20110622050122_change_euthanasia_scheduled_animal.rb
+++
@@ -1,8 +0,0 @@
-class ChangeEuthanasiaScheduledAnimal < ActiveRecord::Migration
-  def self.up
-    rename_column :animals, :euthanasia_scheduled, :euthanasia_date
-  end
-
-  def self.down
-  end
-end
