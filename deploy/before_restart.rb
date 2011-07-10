0fa70571032c8718946c72f5aba4b99dc7459a65
designwaves <brian.bokor@designwaves.com>
2011-07-10 10:43:09 -0400

    updated deploy hooks
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,10 @@
+# Install Jammit Gem
+run "git clone git://github.com/kmamykin/jammit-s3.git"
+run "cd jammit-s3"
+run "git checkout -b aws-s3 remotes/origin/aws-s3 && gem build jammit-s3.gemspec && sudo gem install jammit-s3-0.6.0.2.gem"
+
+# Run Jammit-s3
 run "cd #{current_path} && jammit-s3"
+
+# Upload Recipes
+# run "ey recipes upload -e #{Rails.env}"
