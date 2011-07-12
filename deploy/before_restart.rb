2a3c258542e835378bb3ae4d5615ee82277a9ab8
designwaves <brian.bokor@designwaves.com>
2011-07-11 21:27:21 -0400

    Adding new deploy hooks
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,15 +1,9 @@
-# files = []
-#     files << ['assets']
-#     files << ['javascripts', 'compiled']
-#     files << ['stylesheets', 'compiled']
-#     files = files.map { |path| Dir[Rails.root.join('public', *path, '*.*')] }.flatten
-#     
-#     puts "Removing:"
-#     files.each do |file|
-#       puts "  #{file.gsub(Rails.root.to_s + '/', '')}"
-#     end
-#     
-#     File.delete *files
+# Set Current TimeZone
+run "ln -sf /usr/share/zoneinfo/US/Eastern /usr/share/zoneinfo/localtime"
+
+# Remove Generated Assets
+sudo "rm #{current_path}/public/assets/*"
+
 # Run Jammit-s3
 run "cd #{current_path} && bundle exec jammit-s3"
 
