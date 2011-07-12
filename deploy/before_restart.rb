0cfbcdd8bf60423e43784cfd0b9a591a601aaa6d
designwaves <brian.bokor@designwaves.com>
2011-07-11 20:50:01 -0400

    removed assets not needed and added a demo environment.
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,3 +1,15 @@
+# files = []
+#     files << ['assets']
+#     files << ['javascripts', 'compiled']
+#     files << ['stylesheets', 'compiled']
+#     files = files.map { |path| Dir[Rails.root.join('public', *path, '*.*')] }.flatten
+#     
+#     puts "Removing:"
+#     files.each do |file|
+#       puts "  #{file.gsub(Rails.root.to_s + '/', '')}"
+#     end
+#     
+#     File.delete *files
 # Run Jammit-s3
 run "cd #{current_path} && bundle exec jammit-s3"
 
