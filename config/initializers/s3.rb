3abe9a794105bfb6bca144c05e2b8de3db2bfdb4
designwaves <brian.bokor@designwaves.com>
2011-07-26 14:54:11 -0400

    Updated s3 params to be a bit cleaner and open to updates.
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,15 +1,14 @@
 require "yaml"
 
-S3_CREDENTIALS = YAML.load_file(Rails.root.join("config/s3.yml"))
+S3_CREDENTIALS = YAML.load_file(Rails.root.join("config/s3.yml"))[Rails.env]
 
 require "aws/s3"
 
-S3_BUCKET      = S3_CREDENTIALS[Rails.env]["bucket"] 
-S3_ACL         = S3_CREDENTIALS[Rails.env]["acl"]
+S3_BUCKET      = S3_CREDENTIALS["bucket"] 
+S3_ACL         = S3_CREDENTIALS["acl"]
 S3_CONNECTED   = AWS::S3::Base.connected?
 
-unless AWS::S3::Base.connected?
-  AWS::S3::Base.establish_connection!(
-          :access_key_id => S3_CREDENTIALS["common"]["access_key_id"],
-          :secret_access_key => S3_CREDENTIALS["common"]["secret_access_key"])
+unless S3_CONNECTED
+  AWS::S3::Base.establish_connection!(:access_key_id => S3_CREDENTIALS["access_key_id"],
+                                      :secret_access_key => S3_CREDENTIALS["secret_access_key"])
 end
