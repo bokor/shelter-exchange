03feb6b370ad862a1bd8b60c6181d47115aa81bf
designwaves <brian.bokor@designwaves.com>
2011-08-05 23:10:14 -0400

    removing comments from file
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/models/animal_status.rb
+++ app/models/animal_status.rb
@@ -21,17 +21,3 @@ class AnimalStatus < ActiveRecord::Base
   
 end
 
-# <option value="1">Available for Adoption</option>
-# <option value="2">Adopted</option>
-# <option value="3" selected="selected">Foster Care</option>
-# <option value="4">New Intake</option>
-# <option value="5">In Transit</option>
-# <option value="6">Rescue Candidate</option>
-# <option value="7">Stray Intake</option>
-# <option value="8">On Hold - Behavioral</option>
-# <option value="9">On Hold - Medical</option>
-# <option value="10">On Hold - Bite</option>
-# <option value="11">On Hold - Custody</option>
-# <option value="12">Reclaim</option>
-# <option value="13">Deceased</option>
-# <option value="14">Euthanized</option></select>
