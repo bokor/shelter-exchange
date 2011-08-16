cdc4b9ce2179e75fb23bf753d98b8c505ab4dc25
designwaves <brian.bokor@designwaves.com>
2011-08-15 21:46:04 -0400

    updated ey file and added print link
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/controllers/api/animals_controller.rb
+++ app/controllers/api/animals_controller.rb
@@ -2,8 +2,12 @@ class Api::AnimalsController < Api::ApplicationController
   respond_to :html, :json, :xml
     
   def index
+    # types = params[:types].split(",").collect{|x| x.to_i } if params[:types]
+    # statuses = params[:statuses].split(",").collect{|x| x.to_i } if params[:statuses]
+    # logger.debug("ANIMAL TYPES :::: #{types}")
+    # logger.debug("ANIMAL STATUSES :::: #{statuses}")
     unless request.format.html?
-      @animals = @current_shelter.animals.includes(:animal_type, :animal_status).available_for_adoption
+      @animals = @current_shelter.animals.includes(:animal_type, :animal_status).available_for_adoption #.api_list(types, statuses)
     else
       @animals = @current_shelter.animals.includes(:animal_type, :animal_status).available_for_adoption.paginate(:per_page => Animal::PER_PAGE_API, :page => params[:page])
     end
