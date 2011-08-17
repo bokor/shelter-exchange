b5bc9b9b1bf6c08da40b42e1ba1f70bc1fe1984a
designwaves <brian.bokor@designwaves.com>
2011-08-17 11:08:19 -0400

    Fixing deployment files
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -2,7 +2,8 @@
 sudo "ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime"
 
 # Run Jammit-s3
-run "cd #{current_path} && bundle exec jammit-s3 --config config/asset_#{environment}.yml"
+config_file = (environment.production?) ? "config/assets.yml" : "config/assets_#{environment}.yml"
+run "cd #{current_path} && bundle exec jammit-s3 --config #{config_file}"
 
 # Restart Delayed Job
 sudo "monit -g dj_shelter_exchange_app restart all"
