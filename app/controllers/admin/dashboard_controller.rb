9d6ee16e356bef9fc8737b21e163b45ae4698d94
designwaves <brian.bokor@designwaves.com>
2011-08-24 13:58:06 -0400

    added transfer count to admin dashboard
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < Admin::ApplicationController
   
   def index
     @counts_by_status = Animal.unscoped.joins(:animal_status).group("animal_statuses.name").limit(nil).count
+    @counts_by_transfer = Transfer.where(:status => Transfer::COMPLETED).count
   end
   
 end
