210911fb85dfde57ef1f8b2558850c2b4d255cb8
designwaves <brian.bokor@designwaves.com>
2011-08-25 01:17:53 -0400

    removed cache-control...doesn't seem to be working
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,3 @@ ShelterExchangeApp::Application.initialize!
 
 # Makes the errors blank
 ActionView::Base.field_error_proc = proc { |input, instance| input }
-
-# Added to support s3 cache control
-require "aws/s3"
-require "s3_cache_control"
