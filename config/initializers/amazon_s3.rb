d5b0cba9f6649349effacb08b3f4004a3f320585
designwaves <brian.bokor@designwaves.com>
2011-08-25 01:04:56 -0400

    Moved design to public and updated caching again
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/initializers/amazon_s3.rb
+++ config/initializers/amazon_s3.rb
@@ -13,19 +13,17 @@ unless S3_CONNECTED
                                       :secret_access_key => S3_CREDENTIALS["secret_access_key"])
 end
 
+
 #  AWS:S3 Cache Control Patch
 module AWS
   module S3
     class S3Object
       class << self
-        def store_with_cache_control(key, data, bucket = nil, options = {})
+        def store(key, data, bucket = nil, options = {})
           options["Cache-Control"] = 'max-age=315360000' if options["Cache-Control"].blank?
           options["Expires"]       = 315360000.from_now.httpdate if options["Expires"].blank?
-          
-          store_without_cache_control(key, data, bucket, options)
+          super
         end
-
-        alias_method_chain :store, :cache_control
       end
     end
   end
