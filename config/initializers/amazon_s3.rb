c902b88f67902921bab0d5cb6d9669ffaa3b6f5e
designwaves <brian.bokor@designwaves.com>
2011-08-25 01:10:34 -0400

    updated to fix cache control
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/initializers/amazon_s3.rb
+++ config/initializers/amazon_s3.rb
@@ -12,19 +12,3 @@ unless S3_CONNECTED
   AWS::S3::Base.establish_connection!(:access_key_id => S3_CREDENTIALS["access_key_id"],
                                       :secret_access_key => S3_CREDENTIALS["secret_access_key"])
 end
-
-
-#  AWS:S3 Cache Control Patch
-module AWS
-  module S3
-    class S3Object
-      class << self
-        def store(key, data, bucket = nil, options = {})
-          options["Cache-Control"] = 'max-age=315360000' if options["Cache-Control"].blank?
-          options["Expires"]       = 315360000.from_now.httpdate if options["Expires"].blank?
-          super
-        end
-      end
-    end
-  end
-end
---
+++ lib/s3_cache_control.rb
@@ -0,0 +1,16 @@
+module AWS
+  module S3
+    class S3Object
+      class << self
+        def store_with_cache_control(key, data, bucket = nil, options = {})
+          options["Cache-Control"] = 'max-age=315360000' if options["Cache-Control"].blank?
+          options["Expires"]       = 315360000.from_now.httpdate if options["Expires"].blank?
+          
+          store_without_cache_control(key, data, bucket, options)
+        end
+
+        alias_method_chain :store, :cache_control
+      end
+    end
+  end
+end
