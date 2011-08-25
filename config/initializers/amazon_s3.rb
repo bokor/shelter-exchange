dd43c32bb2707e513ef18170a21043c550595194
designwaves <brian.bokor@designwaves.com>
2011-08-25 00:47:29 -0400

    fix cache control and expires from s3 initializer
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/initializers/amazon_s3.rb
+++ config/initializers/amazon_s3.rb
@@ -19,8 +19,8 @@ module AWS
     class S3Object
       class << self
         def store_with_cache_control(key, data, bucket = nil, options = {})
-          options[:cache_control] = 'max-age=315360000' if options[:cache_control].blank?
-          options[:expires]       = 315360000.from_now.httpdate if options[:expries].blank?
+          options["Cache-Control"] = 'max-age=315360000' if options["Cache-Control"].blank?
+          options["Expires"]       = 315360000.from_now.httpdate if options["Expires"].blank?
           
           store_without_cache_control(key, data, bucket, options)
         end
