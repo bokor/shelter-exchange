a52cb63afc8cee67b2edd8f3c8cb50a3855f6d65
designwaves <brian.bokor@designwaves.com>
2011-08-26 12:40:41 -0400

    Fixing production and staging code
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/initializers/delayed_jobs.rb
+++ config/initializers/delayed_jobs.rb
@@ -3,8 +3,8 @@ Delayed::Worker.destroy_failed_jobs = false
 #Delayed::Worker.sleep_delay = 60
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 5.minutes
-if Rails.env.staging? or Rails.env.demo? or Rails.env.test?
-  Delayed::Worker.delay_jobs = false
-else
-  Delayed::Worker.delay_jobs = true
-end
+# if Rails.env.staging? or Rails.env.demo? or Rails.env.test?
+#   Delayed::Worker.delay_jobs = false
+# else
+#   Delayed::Worker.delay_jobs = true
+# end
--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -7,14 +7,7 @@ run "cd #{current_path} && bundle exec jammit-s3 --config #{config_file}"
 
 # Restart Delayed Job
 sudo "monit restart all dj_shelter_exchange_app"
-# sudo "monit -g dj_shelter_exchange_app restart all" OLD WAY
 
-# run "cd #{current_path} && script/delayed_job restart"
-# $ RAILS_ENV=production script/delayed_job start
-# $ RAILS_ENV=production script/delayed_job stop
-# # Runs two workers in separate processes.
-# $ RAILS_ENV=production script/delayed_job -n 2 start
-# $ RAILS_ENV=production script/delayed_job stop
 
 
 
