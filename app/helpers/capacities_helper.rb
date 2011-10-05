1e6ba60c94cd7853ef8244bce81dac2ff856e934
designwaves <brian.bokor@designwaves.com>
2011-10-04 21:34:25 -0400

    fixed a capacity bug where it wasn't calculating the thresholds
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/helpers/capacities_helper.rb
+++ app/helpers/capacities_helper.rb
@@ -1,13 +1,12 @@
 module CapacitiesHelper
   
   def capacity_status(capacity, animal_count)
-    if animal_count >=  (capacity.max_capacity * 0.6).round.to_i and animal_count <= (capacity.max_capacity * 0.8).round.to_i
-  		return "yellow"  
-  	elsif animal_count >= capacity.max_capacity
+    if (animal_count >= (capacity.max_capacity * 0.8).round.to_i)
   		return "red"
-  	else 
-  		return "green"
+    elsif (animal_count >= (capacity.max_capacity * 0.6).round.to_i) and (animal_count < (capacity.max_capacity * 0.8).round.to_i)
+  		return "yellow"
+    elsif (animal_count < (capacity.max_capacity * 0.6).round.to_i)
+      return "green"
   	end
   end
-  
 end
