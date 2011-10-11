dbc36ed09a7cdc1f9e8471861694e7a445ffdeef
designwaves <brian.bokor@designwaves.com>
2011-10-11 01:08:11 -0400

    Testing update with env variable in environment.rb
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,5 @@ ShelterExchangeApp::Application.initialize!
 
 # Makes the errors blank
 ActionView::Base.field_error_proc = proc { |input, instance| input }
+
+ENV["RAILS_ASSET_ID"]=Time.now.to_time.i
