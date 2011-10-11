c1141bf17321704a0991b802ae30f92a03cabf37
designwaves <brian.bokor@designwaves.com>
2011-10-11 01:19:43 -0400

    updating deployment
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -3,7 +3,7 @@ sudo "ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime"
 
 # Run Jammit-s3
 config_file = (environment == "production") ? "config/assets.yml" : "config/assets_#{environment}.yml"
-run "cd #{current_path} && RAILS_ASSET_ID=#{revision} && bundle exec jammit-s3 --config #{config_file}"
+run "cd #{current_path} && RAILS_ASSET_ID=20110120051234 && bundle exec jammit-s3 --config #{config_file}"
 
 # Restart Delayed Job
 sudo "monit restart all dj_shelter_exchange_app"
