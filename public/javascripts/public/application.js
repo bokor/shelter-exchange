fe13e37dea1b29bdcb96cec93f2bdc0935d83b31
designwaves <brian.bokor@designwaves.com>
2011-10-10 23:03:50 -0400

    Fixed pagination issues
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- public/javascripts/public/application.js
+++ public/javascripts/public/application.js
@@ -50,3 +50,53 @@ $(function() {
 
 });
 
+
+/* Block UI - Setup and Config - Shows up if > than 400 ms
+/*----------------------------------------------------------------------------*/ 
+$(function() {
+	var timer;
+	
+  	$(document).ajaxStart(function(){
+  		timer = setTimeout(function(){
+        	timer = null;
+       		$("#main").block({
+				message:  '<div class="ajax-message">' +
+						  '<p><img src="/images/ajax-loader.gif" width="40" height="40" /></p>' +
+						  '<h1>Loading . . .</h1>' +
+						  '</div>',
+				centerY: false, 
+				showOverlay: false,
+				css: {
+			        padding:        		 '15px',
+			        width:          		 '150px',
+					height: 				 '90px',
+			        top:                     ($(window).scrollTop() + 200) + 'px',
+			        border:         		 '0px',
+			        backgroundColor: 		 '#000',
+					'-webkit-border-radius': '10px', 
+			    	'-moz-border-radius':    '10px',
+					'border-radius':         '10px',
+					opacity:        		 0.7,
+					color:          		 '#fff'
+			    },
+			    overlayCSS:  {
+			        backgroundColor: 		 '#000',
+			        opacity:         		 0.7
+			    }
+			});
+     	}, 400);
+ 	}).ajaxStop( function() {
+   		clearTimeout(timer);
+       	timer = null;
+    	$("#main").unblock();
+	});
+});
+
+/* Pagination Links :: AJAX Searches
+/*----------------------------------------------------------------------------*/
+$('.pagination a').live('click',function (e){  
+	$.getScript(this.href);  
+    // return false;  
+	e.preventDefault();
+});
+
