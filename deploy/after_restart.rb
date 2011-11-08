fd70ac8777a2f0c1390ea6aceb17f83efe9bbbd1
designwaves <brian.bokor@designwaves.com>
2011-11-08 13:47:29 -0500

    Updated deployment hooks
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,2 +1,10 @@
-# Remove Generated Assets
-# sudo "rm -rf #{current_path}/public/assets/*"
+# Remove Jammit-S3 - Generated Assets
+sudo "rm -rf #{current_path}/public/assets/*"
+
+# Restart Delayed Job
+sudo "monit -g dj_shelter_exchange_app restart all"
+
+# Ping Search Engines - Updated Sitemap.xml
+# if environment == "production"
+#   ping
+# end
--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -5,9 +5,6 @@ sudo "ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime"
 config_file = (environment == "production") ? "config/assets.yml" : "config/assets_#{environment}.yml"
 run "cd #{current_path} && bundle exec jammit-s3 --config #{config_file}"
 
-# Restart Delayed Job
-sudo "monit restart all dj_shelter_exchange_app"
-
 
 
 
