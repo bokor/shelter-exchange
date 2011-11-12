f512247c5876b47a8c41764305155240c1dedde9
designwaves <brian.bokor@designwaves.com>
2011-11-12 15:38:36 -0500

    Removed the code and moved into the comments
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,8 +1,6 @@
 class Public::ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  # before_filter :authenticate! if Rails.env.demo? or Rails.env.staging?
-  
+    
   layout :current_layout
 
   private
@@ -10,11 +8,14 @@ class Public::ApplicationController < ActionController::Base
     def current_layout
       'public/application'
     end
-    
-    # def authenticate!
-    #   authenticate_or_request_with_http_basic do |username, password|
-    #     username == "shelterexchange" && password == "sav1ngl1ves"
-    #   end
-    # end
 
 end
+
+# MOVED TO NGINX CONFIG
+# before_filter :authenticate! if Rails.env.demo? or Rails.env.staging?  
+  
+# def authenticate!   
+#   authenticate_or_request_with_http_basic do |username, password|
+#     username == "shelterexchange" && password == "sav1ngl1ves"
+#   end
+# end
