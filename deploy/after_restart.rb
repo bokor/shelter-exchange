8d1a6717264ac713fcd9f9efdad2ee87776894a9
designwaves <brian.bokor@designwaves.com>
2011-11-13 16:56:07 -0500

    Updated deploy hooks to try and fix the jammit-s3 stuff
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,5 +1,5 @@
 # Remove Jammit-S3 - Generated Assets
-sudo "rm -rf #{current_path}/public/assets/*"
+# sudo "rm -rf #{current_path}/public/assets/*"
 
 # Restart Delayed Job
 if environment == "production"
@@ -7,8 +7,3 @@ if environment == "production"
 elsif ["staging"].include?(environment)
   sudo "monit -g dj_shelter_exchange_app restart all"
 end
-  
-# Ping Search Engines - Updated Sitemap.xml
-# if environment == "production"
-#   on_utilities("shelter_exchange_util"){ run "cd #{current_path} && bundle exec rake sitemap:ping" }
-# end
