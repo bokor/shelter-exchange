6751c2049337816351ac3530d0f56b26f661ae8e
designwaves <brian.bokor@designwaves.com>
2011-11-13 17:59:02 -0500

    Updated deploy hooks
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -2,15 +2,5 @@
 sudo "ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime"
 
 # Run Jammit-s3
-case environment
-when "production"
-  on_app_master(){ run "cd #{current_path} && bundle exec jammit-s3 --config config/assets.yml" }
-when "staging"
-  run "cd #{current_path} && bundle exec jammit-s3 --config config/assets_staging.yml"
-end
-
-
-
-
-
-
+config_file = (environment == "production" ? "config/assets.yml" : "config/assets_#{environment}.yml")
+on_app_master(){ run "cd #{current_path} && bundle exec jammit-s3 --config #{config_file}" }
