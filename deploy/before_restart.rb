7d5d0d5440c1bf31a4471fab5052662438ecf1eb
designwaves <brian.bokor@designwaves.com>
2011-11-13 17:17:58 -0500

    updated config file
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -3,7 +3,7 @@ sudo "ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime"
 
 # Run Jammit-s3
 config_file = (environment == "production" ? "config/assets.yml" : "config/assets_#{environment}.yml")
-on_app_master(){ run "cd #{current_path} && bundle exec jammit-s3 --config config/assets.yml" }
+on_app_master(){ run "cd #{current_path} && bundle exec jammit-s3 --config #{config_file}" }
 
 
 # if environment == "production"
