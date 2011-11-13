b8236b61d1e29491f159c9d043d7d3089e7fe424
designwaves <brian.bokor@designwaves.com>
2011-11-13 17:52:45 -0500

    Updated deploy hook for testing
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -4,7 +4,7 @@ sudo "ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime"
 # Run Jammit-s3
 case environment
 when "production"
-  on_app_master(){ run "cd #{current_path} && bundle exec jammit-s3 --config config/assets.yml" }
+  on_utilities("shelter_exchange_util"){ run "cd #{current_path} && bundle exec jammit-s3 --config config/assets.yml" }
 when "staging"
   run "cd #{current_path} && bundle exec jammit-s3 --config config/assets_staging.yml"
 end
