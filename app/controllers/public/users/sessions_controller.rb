d1b0c095b17ce0e12b8aee7dc355f7c85968aa66
designwaves <brian.bokor@designwaves.com>
2011-12-11 15:34:47 -0500

    Updated the login system
    fix the confirmation code and confirm within
    
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/controllers/public/users/sessions_controller.rb
+++ app/controllers/public/users/sessions_controller.rb
@@ -3,7 +3,8 @@ class Public::Users::SessionsController < ::Devise::SessionsController
   layout 'public/application'
   
   def new  
-    sign_out(:user) # Added because we aren't allowing to be signed into WWW site.    
+    # Force Signout because www is not allowed to have a sign in per se
+    sign_out(:user) 
   end
   
   def create        
@@ -19,71 +20,17 @@ class Public::Users::SessionsController < ::Devise::SessionsController
     
     # Try to sign in the user and redirect to subdomain URL
     sign_in_and_redirect(:user, resource)
-  end 
+  end
   
-  def sign_in_and_redirect(resource_or_scope, resource=nil)
-    current_user.authentication_token = Devise.friendly_token
-    current_user.save
-    sign_out(:user)
- 
-    redirect_to valid_token_user_url(current_user.authentication_token, :subdomain => current_user.account.subdomain )
-  end  
+  
+  private
+    
+    def sign_in_and_redirect(resource_or_scope, resource=nil)
+      auth_token = Devise.friendly_token
+      current_user.update_attributes(:authentication_token => auth_token)
+    
+      sign_out(:user)
+      redirect_to valid_token_user_url(auth_token, :subdomain => current_user.account.subdomain)
+    end  
   
 end
-
-# resource = warden.authenticate!(:scope => :user, :recall => "#{controller_path}#new")
-# set_flash_message(:notice, :signed_in) if is_navigational_format?
-# sign_in(:user, resource)
-
-# params[:user][:subdomain] = @subdomain 
-# resource = User.find_for_authentication
-
-# def sign_in(resource_or_scope, resource=nil)
-#   super
-#   sign_in_and_redirect(:user, resource)
-# end
-
-
-# class Public::Users::SessionsController < ::Devise::SessionsController
-#   include UrlHelper
-#   layout 'public/application'
-#   
-#   def new  
-#     # sign_out(:user) # Added because we aren't allowing to be signed into WWW site.
-#     Devise.sign_out_all_scopes ? sign_out : sign_out(:user)
-#     set_flash_message :notice, :signed_out if signed_in?(:user)
-#   end
-#   
-#   def create 
-#     Devise.mappings[:user].controllers[:sessions] = "public/users/sessions"
-#     user = User.where(:email => params[:user][:email]).first
-#     
-#     if user.present? 
-#       if user.confirmed?
-#         params[:user][:subdomain] = user.account.subdomain
-#         resource = warden.authenticate!(:scope => :user)
-#         sign_in_and_redirect(:user, resource)
-#       else
-#         not_confirmed = true
-#       end
-#     end
-#     
-#     set_flash_message :notice, not_confirmed ? :unconfirmed : :invalid
-#     render :new
-#     
-#   end 
-# 
-#   
-#   def sign_in_and_redirect(resource_or_scope, resource=nil)
-#     scope = Devise::Mapping.find_scope!(resource_or_scope)
-#     resource ||= resource_or_scope
-#     sign_in(scope, resource) unless warden.user(scope) == resource
-#     
-#     current_user.authentication_token = Devise.friendly_token
-#     current_user.save
-#     sign_out(:user)
-#     
-#     redirect_to valid_token_user_url(current_user.authentication_token, :subdomain => current_user.account.subdomain) && return
-#   end  
-#   
-# end
