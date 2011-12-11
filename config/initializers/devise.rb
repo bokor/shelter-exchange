e154dd529ef9fc04ee2fa2072c4af3b62d97ec8f
designwaves <brian.bokor@designwaves.com>
2011-12-11 15:37:09 -0500

    Fixed the invitation from 1 week to 0
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -70,7 +70,7 @@ Devise.setup do |config|
   # The period the generated invitation token is valid, after
   # this period, the invited resource won't be able to accept the invitation.
   # When invite_for is 0 (the default), the invitation won't expire.
-  config.invite_for = 1.weeks
+  config.invite_for = 0
   
   # Number of invitations users can send.
   # If invitation_limit is nil, users can send unlimited invitations.
