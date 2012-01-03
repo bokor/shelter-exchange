166cc17b51fdac3864cb4bc59d54020aa3d4368b
designwaves <brian.bokor@designwaves.com>
2012-01-02 20:41:29 -0500

    Updates current year in www footer and the ping search engines on deploy
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,9 +1,11 @@
-# Remove Jammit-S3 - Generated Assets  SEEMS TO CAUSE ISSUES WITH THE ?date not to be added on the urls
-# on_app_master(){ sudo "rm -rf #{current_path}/public/assets/*" }
-
 # Restart Delayed Job
 if environment == "production"
   on_utilities("shelter_exchange_util"){ sudo "monit -g dj_shelter_exchange_app restart all" }
 elsif ["staging"].include?(environment)
   sudo "monit -g dj_shelter_exchange_app restart all"
+end
+
+# Ping Search Engines - Updated Sitemap.xml
+if environment == "production"
+  on_utilities("shelter_exchange_util"){ run "cd #{current_path} && bundle exec rake sitemap:ping" }
 end
