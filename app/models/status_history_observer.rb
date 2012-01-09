bd4ad8666145d92d2ccdb28fee5afe5cfaa57934
designwaves <brian.bokor@designwaves.com>
2012-01-09 09:37:07 -0500

    Updated tweet function
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/models/status_history_observer.rb
+++ app/models/status_history_observer.rb
@@ -7,7 +7,14 @@ class StatusHistoryObserver < ActiveRecord::Observer
   private
   
     def send_tweet(animal, shelter)
-      Delayed::Job.enqueue(TweetJob.new(animal, shelter)) if animal.available_for_adoption?
+      url = Googl.shorten("http://www.shelterexchange.org/save_a_life/#{animal.id}").short_url
+      type = " #{indefinite_articlerize(animal.animal_type.name)}" unless animal.other?
+      tweet = "Adopt#{type} - #{animal.name}, #{indefinite_articlerize(animal.full_breed)} in ##{shelter.city} ##{shelter.state} #{shelter.twitter} #saveanimals #{url}"
+      Delayed::Job.enqueue(TweetJob.new(tweet)) if animal.available_for_adoption? and tweet.length <= 140
+    end
+    
+    def indefinite_articlerize(params_word)
+        %w(a e i o u).include?(params_word[0].downcase) ? "an #{params_word}" : "a #{params_word}"
     end
 
 end
--- lib/tweet_job.rb
+++ lib/tweet_job.rb
@@ -1,15 +1,7 @@
-class TweetJob < Struct.new(:animal, :shelter)
+class TweetJob < Struct.new(:tweet)
   
   def perform
-    url = "http://www.shelterexchange.org/save_a_life/#{animal.id}"
-    type = " #{indefinite_articlerize(animal.animal_type.name)}" unless animal.other?
-    Twitter.update("Adopt#{type} - #{animal.name}, #{indefinite_articlerize(animal.full_breed)} in ##{shelter.city} ##{shelter.state} #{shelter.twitter} #saveanimals #{url}")
+    Twitter.update(tweet)
   end    
-  
-  private
-    
-    def indefinite_articlerize(params_word)
-        %w(a e i o u).include?(params_word[0].downcase) ? "an #{params_word}" : "a #{params_word}"
-    end
   
 end
