c72c351f6880bb655e96709b8acffb78352d6ee4
designwaves <brian.bokor@designwaves.com>
2012-01-09 20:12:58 -0500

    Updated parents text and added welcome email with delay
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/mailers/account_mailer.rb
+++ app/mailers/account_mailer.rb
@@ -1,14 +1,18 @@
 class AccountMailer < ActionMailer::Base
-  default :from => "ShelterExchange <do-not-reply@shelterexchange.org>",
-          :to => "application@shelterexchange.org"
-          
-
+  default :from => "ShelterExchange <do-not-reply@shelterexchange.org>"
+      
   def account_created(account, shelter, user)
     @account = account
     @shelter = shelter
     @user = user
-    mail(:subject => "Shelter Exchange [#{Rails.env}] - A new account has been created (#{shelter.name})")
+    mail(:to => "application@shelterexchange.org",
+         :subject => "Shelter Exchange [#{Rails.env}] - A new account has been created (#{shelter.name})")
   end
-
+  
+  def welcome(user)
+    @user = user
+    attachments.inline["logo_email.jpg"] = File.read(Rails.root.join('public/images/logo_email.jpg'))
+    mail(:to => @user.email, :subject => "Welcome to Shelter Exchange!")   
+  end
 
 end
--- app/models/account_observer.rb
+++ app/models/account_observer.rb
@@ -2,6 +2,7 @@ class AccountObserver < ActiveRecord::Observer
   
   def after_create(account)
     account_created_notification(account)
+    welcome_notification(account)
   end
   
   
@@ -9,6 +10,11 @@ class AccountObserver < ActiveRecord::Observer
     
     def account_created_notification(account)
       AccountMailer.delay.account_created(account,account.shelters.first,account.users.first)
+    end
+    
+    def welcome_notification(account)
+      run_at = Rails.env.production? ? 1.hour.from_now : Time.zone.now
+      AccountMailer.delay(:run_at => run_at).welcome(account.users.first)
     end
 
 end
