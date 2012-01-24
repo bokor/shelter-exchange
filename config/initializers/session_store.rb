7659b5122d83943f1f9ed352a768982821c6fe0c
designwaves <brian.bokor@designwaves.com>
2012-01-24 17:36:56 -0500

    Testing staging login with the new domain in the session store
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-ShelterExchangeApp::Application.config.session_store :cookie_store, :key => '_shelterexchange_session', :domain => "shelterexchange.org"
+ShelterExchangeApp::Application.config.session_store :cookie_store, :key => '_shelterexchange_session', :domain => "shelterexchange-staging.org"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
