de38cc4855b5d6ca6270cefd0ba3ecf260d611d6
designwaves <brian.bokor@designwaves.com>
2012-03-08 15:15:13 -0500

    Updated Facebook Linter, Save a life and help a shelter image links and sign up page subdomain
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- lib/jobs/facebook_linter_job.rb
+++ lib/jobs/facebook_linter_job.rb
@@ -5,3 +5,13 @@ class Jobs::FacebookLinterJob < Struct.new(:id)
   end
 
 end
+
+# https://graph.facebook.com?id=http://www.shelterexchange.org/save_a_life/647&scrape=true
+#https://developers.facebook.com/tools/lint/?url={YOUR_URL}&format=json
+
+# curl -X POST \
+#      -F "id=http://www.shelterexchange.org/save_a_life/1238" \
+#      -F "scrape=true" \
+#      "https://graph.facebook.com"
+
+# https://graph.facebook.com?id=http://www.shelterexchange.org/save_a_life/1238&scrape=true
