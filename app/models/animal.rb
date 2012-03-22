b5db10d3fe1025afece463cead7348a0b9d239a0
designwaves <brian.bokor@designwaves.com>
2012-03-21 22:41:45 -0400

    Updated notes and status histories to correct shelter
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -228,12 +228,14 @@ class Animal < ActiveRecord::Base
     self.status_history_reason = "Transferred from #{current_shelter.name}"
     self.status_change_date = Date.today
     self.shelter_id = requestor_shelter.id
-    self.arrival_date = nil
+    self.arrival_date = Date.today
     self.hold_time = nil
     self.euthanasia_date = nil
     self.accommodation_id = nil
     self.touch(:updated_at)
     self.save(:validate => false)
+    self.notes.update_all({:shelter_id => requestor_shelter.id})
+    self.status_histories.update_all({:shelter_id => requestor_shelter.id})
     self.tasks.delete_all
     self.alerts.delete_all
   end
