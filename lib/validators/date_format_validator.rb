44b8d59b04258d2d578c251e0026dd369c31c2fc
designwaves <brian.bokor@designwaves.com>
2012-04-05 15:19:42 -0400

    Updated the date validation to have a specific validation for date_of_birth
    Signed-off-by: designwaves <brian.bokor@designwaves.com>

--- lib/validators/date_format_validator.rb
+++ lib/validators/date_format_validator.rb
@@ -6,12 +6,21 @@ class DateFormatValidator < ActiveModel::EachValidator
     
     record[attribute] = nil 
     unless year.blank? and month.blank? and day.blank?
+      
       begin
-        # raise ArgumentError if year.length < 4 
-        record[attribute] = Date.parse("#{year.to_i}/#{month.to_i}/#{day.to_i}") #Date.civil(year.to_i, month.to_i, day.to_i)
-      rescue ArgumentError
-        record.errors.add(attribute, options[:message] || "is an invalid date format")
+        date = Date.parse("#{year.to_i}/#{month.to_i}/#{day.to_i}")
+        raise ArgumentError, :date_of_birth if attribute == :date_of_birth && date > Date.today
+        record[attribute] = date 
+      rescue ArgumentError => e
+        if e.message == :date_of_birth
+          record.errors.add(attribute, options[:message] || "has to be before today's date") 
+        else
+          record.errors.add(attribute, options[:message] || "is an invalid date format")
+        end
       end
+      
     end
   end
 end
+
+#Date.civil(year.to_i, month.to_i, day.to_i)
