0ce98600b66b9d48a67959ac4e1c03d2dca926f2
Brian Bokor <brian.bokor@me.com>
2012-12-27 21:54:35 -0800

    renamed initializer files

--- config/initializers/1_setup_fog_connection.rb
+++ config/initializers/1_setup_fog_connection.rb
@@ -18,24 +18,3 @@ FOG_STORAGE ||= Fog::Storage.new({
   :aws_secret_access_key => S3_SECRET_ACCESS_KEY
 })
 FOG_BUCKET ||= FOG_STORAGE.directories.get(S3_BUCKET)
-
-
-
-# CarrierWave Fog Connection
-#----------------------------------------------------------------------------
-require 'carrierwave/orm/activerecord'
-require 'carrierwave/processing/mime_types'
-
-CarrierWave.configure do |config|
-  config.fog_credentials = { 
-    :provider => 'AWS',
-    :aws_access_key_id => S3_ACCESS_KEY_ID, 
-    :aws_secret_access_key => S3_SECRET_ACCESS_KEY 
-  }
-  config.fog_directory  = S3_BUCKET
-  config.fog_attributes = {
-    'Cache-Control' => 'max-age=315576000',
-    'Expires' => 1.year.from_now.httpdate
-  } 
-  config.fog_public = true
-end
