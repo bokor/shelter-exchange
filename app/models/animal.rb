d2b0899e26b359ba4efe1ff00dba836d937d90b2
Brian Bokor <brian.bokor@gmail.com>
2013-01-27 01:00:46 -0800

    updated the animal model to change the breed names based on the breed model if the use has typed in a non-exact match

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,29 +3,30 @@ class Animal < ActiveRecord::Base
   include Statusable, Typeable, Uploadable
   # Animal Namespaced
   include Reportable, Searchable, Mappable, Transferrable, Apiable
-  
+
   default_scope :order => 'animals.updated_at DESC'
 
   # Constants
   #----------------------------------------------------------------------------
-  SIZES = { 
+  SIZES = {
     :S => "Small",
-    :M => "Medium", 
+    :M => "Medium",
     :L => "Large",
     :XL => "X-Large"
   }.freeze
 
   AGES = %w(baby young adult senior).freeze
-  
+
 
   # Callbacks
-  #----------------------------------------------------------------------------  
-  before_save :change_status_date!
+  #----------------------------------------------------------------------------
+  before_save :change_status_date!,
+              :update_breed_names # FIXME: Hack to set the name based on what is should be, view can be lowercase
   after_save :create_status_history!
 
   # Getters/Setters
-  #----------------------------------------------------------------------------  
-  attr_accessor :status_history_reason, 
+  #----------------------------------------------------------------------------
+  attr_accessor :status_history_reason,
                 :date_of_birth_month, :date_of_birth_day, :date_of_birth_year,
                 :arrival_date_month, :arrival_date_day, :arrival_date_year,
                 :euthanasia_date_month, :euthanasia_date_day, :euthanasia_date_year
@@ -43,12 +44,12 @@ class Animal < ActiveRecord::Base
   has_many :tasks, :as => :taskable, :dependent => :destroy
   has_many :status_histories, :dependent => :destroy
   has_many :transfers, :dependent => :destroy
-  
+
   has_many :photos, :as => :attachable, :dependent => :destroy
-  accepts_nested_attributes_for :photos, :limit => Photo::MAX_TOTAL, 
-                                         :allow_destroy => true, 
+  accepts_nested_attributes_for :photos, :limit => Photo::MAX_TOTAL,
+                                         :allow_destroy => true,
                                          :reject_if => Proc.new { |a| a['image'].blank? if a['image_cache'].blank? }
-  
+
   # Validations
   #----------------------------------------------------------------------------
   validates :name, :presence => true
@@ -57,35 +58,35 @@ class Animal < ActiveRecord::Base
   validates :primary_breed, :breed => true
   validates :secondary_breed, :breed => true, :allow_blank => true
   validates :sex, :presence => true
-  validates :age, :presence => { :if => Proc.new { |a| AnimalStatus::ACTIVE.include?(a.animal_status_id) }, 
+  validates :age, :presence => { :if => Proc.new { |a| AnimalStatus::ACTIVE.include?(a.animal_status_id) },
                                  :message => "needs to be selected" }
-  validates :size, :presence => { :if => Proc.new { |a| AnimalStatus::ACTIVE.include?(a.animal_status_id) }, 
+  validates :size, :presence => { :if => Proc.new { |a| AnimalStatus::ACTIVE.include?(a.animal_status_id) },
                                   :message => "needs to be selected" }
-  validates :microchip, :uniqueness => { :allow_blank => true, :scope => :shelter_id, :message => "already exists in your shelter. Please return to the main Animal page and search by this microchip number to locate this record." }  
+  validates :microchip, :uniqueness => { :allow_blank => true, :scope => :shelter_id, :message => "already exists in your shelter. Please return to the main Animal page and search by this microchip number to locate this record." }
   validates :special_needs, :presence => { :if => :special_needs? }
   validates :video_url, :video_url_format => true, :allow_blank => true
   validates :date_of_birth, :date_format => true
   validates :arrival_date, :date_format => true
   validates :euthanasia_date, :date_format => true
-  
-  
-  # Scopes 
+
+
+  # Scopes
   #----------------------------------------------------------------------------
   # scope :with_shelter, includes(:shelter)
   # scope :with_photos, includes(:photos)
   # scope :with_animal_type, includes(:animal_type)
   # scope :with_animal_status, includes(:animal_status)
   scope :latest, lambda { |status, limit| includes(:shelter, :photos).send(status).order("status_change_date DESC").limit(limit) }
-      
-  
+
+
   # Scopes - Dashboard - Recent Activity
   #----------------------------------------------------------------------------
   def self.recent_activity(limit=10)
     includes(:animal_type, :animal_status).reorder("animals.updated_at DESC").limit(limit)
   end
-  #----------------------------------------------------------------------------  
+  #----------------------------------------------------------------------------
 
-  
+
   # Instance Methods
   #----------------------------------------------------------------------------
   def full_breed
@@ -95,45 +96,50 @@ class Animal < ActiveRecord::Base
       self.primary_breed
     end
   end
-  
+
   def special_needs?
     self.has_special_needs
   end
-  
+
   def mix_breed?
     self.is_mix_breed
   end
-  
+
   def sterilized?
     self.is_sterilized
-  end       
-  
+  end
+
   # def photos?
   #   self.photos.present?
   # end
-       
-       
+
+
   # Private Methods
-  #----------------------------------------------------------------------------                                       
+  #----------------------------------------------------------------------------
   private
-  
+
     def is_kill_shelter?
       @shelter ||= self.shelter.kill_shelter?
     end
-            
+
+    def update_breed_names
+      self.primary_breed   = Breed.where(:name => self.primary_breed).first.name
+      self.secondary_breed = Breed.where(:name => self.secondary_breed).first.name
+    end
+
     def change_status_date!
       if self.new_record? or self.animal_status_id_changed?
         self.status_change_date = Date.today
       end
     end
-    
+
     def status_history_reason_required?
       self.animal_status_id.present? and (self.new_record? or self.animal_status_id_changed?)
     end
-    
+
     def create_status_history!
       StatusHistory.create_with(self.shelter_id, self.id, self.animal_status_id, @status_history_reason) if self.new_record? or self.animal_status_id_changed? or self.shelter_id_changed?
     end
-        
+
 end
 
--- lib/shelter_exchange/extensions/validators/breed_validator.rb
+++ lib/shelter_exchange/extensions/validators/breed_validator.rb
@@ -2,7 +2,7 @@ class BreedValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     self.send("validate_#{attribute}", record, attribute, value)
   end
-  
+
   def validate_primary_breed(record, attribute, value)
     unless record.animal_type_id.blank?
       if record.primary_breed.blank?
@@ -16,7 +16,7 @@ class BreedValidator < ActiveModel::EachValidator
       end
     end
   end
-  
+
   def validate_secondary_breed(record, attribute, value)
     unless record.other? # Bypass Type = Other
       if record.mix_breed? and find_breed(value, record.animal_type_id).blank?
@@ -24,37 +24,12 @@ class BreedValidator < ActiveModel::EachValidator
       end
     end
   end
-  
-  private 
-  
+
+  private
+
     def find_breed(breed, animal_type_id)
       Breed.valid_for_animal(breed, animal_type_id)
     end
-  
+
 end
 
-
-
-# def primary_breed_valid?
-#   unless self.animal_type_id.blank?
-#     if self.primary_breed.blank?
-#       errors.add_on_blank(:primary_breed)
-#     else
-#       unless self.other?  # Bypass Type = Other
-#         if Breed.valid_for_animal(self.primary_breed, self.animal_type_id).blank?
-#           errors.add(:primary_breed, "must contain a valid breed from the list")
-#         end
-#       end
-#     end
-#   end
-# end
-# 
-# def secondary_breed_valid?
-#   unless self.other? # Bypass Type = Other
-#     if self.is_mix_breed and self.secondary_breed.present?
-#       if Breed.valid_for_animal(self.secondary_breed, self.animal_type_id).blank?
-#         errors.add(:secondary_breed, "must contain a valid breed from the list")
-#       end
-#     end
-#   end
-# end
