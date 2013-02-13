8e1174073be265f9e9bab33d250cf53e8d96f018
Brian Bokor <brian.bokor@gmail.com>
2013-02-13 01:02:21 -0800

    updated airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = '5c776a2cef9e4c7a23da8c8d2df03b8f'
+  config.api_key = '038a86657017a6f8e031c8a9311e461b'
+  config.host = 'bokor.airbrake.io'
   config.development_environments = ["development", "test"]
 end
