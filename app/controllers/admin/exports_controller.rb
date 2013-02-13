044b6f9e86d100cba53025abf0a272bd1966c04d
Brian Bokor <brian.bokor@gmail.com>
2013-02-12 22:50:25 -0800

    added new export options for Integrations

--- app/controllers/admin/exports_controller.rb
+++ app/controllers/admin/exports_controller.rb
@@ -14,11 +14,52 @@ class Admin::ExportsController < Admin::ApplicationController
     end
   end
 
+  def petfinder_emails
+    shelter_ids = Integration::Petfinder.all.map(&:shelter_id)
+    results = Shelter.select('DISTINCT email').where(:id => shelter_ids).active.all +
+              User.select('DISTINCT users.email').joins(:account => :shelters).where(:shelters => {:status => "active", :id => shelter_ids}).all
+
+    respond_to do |format|
+      format.csv{ send_data(petfinder_emails_csv(results),:filename => "petfinder_emails.csv") }
+    end
+  end
+
+  def adopt_a_pet_emails
+    shelter_ids = Integration::AdoptAPet.all.map(&:shelter_id)
+    results = Shelter.select('DISTINCT email').where(:id => shelter_ids).active.all +
+              User.select('DISTINCT users.email').joins(:account => :shelters).where(:shelters => {:status => "active", :id => shelter_ids}).all
+
+    respond_to do |format|
+      format.csv{ send_data(adopt_a_pet_emails_csv(results),:filename => "adopt_a_pet_emails.csv") }
+    end
+  end
+
+  def all_integrations_emails
+    shelter_ids = Integration.all.map(&:shelter_id)
+    results = Shelter.select('DISTINCT email').where(:id => shelter_ids).active.all +
+              User.select('DISTINCT users.email').joins(:account => :shelters).where(:shelters => {:status => "active", :id => shelter_ids}).all
+
+    respond_to do |format|
+      format.csv{ send_data(all_integrations_email_csv(results),:filename => "all_integration_emails.csv") }
+    end
+  end
+
   private
     def all_emails_csv(results)
       CSV.generate{|csv| results.collect(&:email).uniq.each{|email| csv << [email] } }
     end
 
+    def petfinder_emails_csv(results)
+      CSV.generate{|csv| results.collect(&:email).uniq.each{|email| csv << [email] } }
+    end
+
+    def adopt_a_pet_emails_csv(results)
+      CSV.generate{|csv| results.collect(&:email).uniq.each{|email| csv << [email] } }
+    end
+
+    def all_integrations_email_csv(results)
+      CSV.generate{|csv| results.collect(&:email).uniq.each{|email| csv << [email] } }
+    end
 end
 
 #select distinct shelters.email from shelters
@@ -30,3 +71,5 @@ end
 #left outer join shelters on shelters.`account_id` = accounts.id
 #left outer join integrations on integrations.shelter_id = shelters.id
 #where integrations.type = 'Integration::Petfinder'
+#
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ShelterExchangeApp::Application.routes.draw do
-  
+
   # Shared Functions
   #----------------------------------------------------------------------------
   namespace :shared do
-    
+
     # Shared :: Breeds
     #----------------------------------------------------------------------------
     resources :breeds, :only => [:auto_complete] do
@@ -22,21 +22,21 @@ ShelterExchangeApp::Application.routes.draw do
   # Application - Routes for *subdomain*.shelterexchange.org
   #----------------------------------------------------------------------------
   constraints(ShelterExchange::Subdomains::App) do
-  
+
     # Notes
     #----------------------------------------------------------------------------
     resources :notes, :only => [:create, :edit, :update, :destroy]
-  
+
     # Comments
     #----------------------------------------------------------------------------
     resources :comments, :only => [:create, :edit, :update, :destroy]
-  
+
     # Items
     #----------------------------------------------------------------------------
     resources :items
-  
+
     # Reports
-    #----------------------------------------------------------------------------    
+    #----------------------------------------------------------------------------
     resources :reports do
       collection do
         get :status_by_month_year
@@ -53,7 +53,7 @@ ShelterExchangeApp::Application.routes.draw do
         get :reclaimed_monthly_total_by_type
       end
     end
-  
+
     # Locations
     #----------------------------------------------------------------------------
     resources :locations, :only => [:create, :edit, :update, :destroy] do
@@ -61,7 +61,7 @@ ShelterExchangeApp::Application.routes.draw do
         get :find_all
       end
     end
-  
+
     # Accommodations
     #----------------------------------------------------------------------------
     resources :accommodations, :except => [:show] do
@@ -70,28 +70,28 @@ ShelterExchangeApp::Application.routes.draw do
         get :filter_by_type_location
       end
     end
-  
+
     # Tasks
     #----------------------------------------------------------------------------
     resources :tasks, :except => [:show] do
       post :complete, :on => :member
     end
-  
+
     # Alerts
     #----------------------------------------------------------------------------
     resources :alerts, :except => [:show] do
       post :stop, :on => :member
     end
-  
+
     # Placements
     #----------------------------------------------------------------------------
     resources :placements, :only => [:create, :destroy] do
       get :find_comments, :on => :member
       resources :comments
     end
-  
+
     # Communities
-    #----------------------------------------------------------------------------    
+    #----------------------------------------------------------------------------
     resources :communities do
       collection do
         get :filter_notes
@@ -99,15 +99,15 @@ ShelterExchangeApp::Application.routes.draw do
         get :find_animals_for_shelter
       end
     end
-  
+
     # Maps
     #----------------------------------------------------------------------------
     resources :maps, :only => [:overlay]
-  
+
     # Transfers
     #----------------------------------------------------------------------------
     resources :transfers, :except => [:index, :show]
-  
+
     # Parents
     #----------------------------------------------------------------------------
     resources :parents do
@@ -116,20 +116,20 @@ ShelterExchangeApp::Application.routes.draw do
         get :search
       end
     end
-    
+
     # Photos
     #----------------------------------------------------------------------------
     resources :photos, :only => [:destroy]
-  
+
     # Animals
     #----------------------------------------------------------------------------
     resources :animals do
       resources :notes
-      resources :alerts 
-      resources :tasks 
+      resources :alerts
+      resources :tasks
       resources :photos, :only => [:create, :destroy] do
         get :refresh_gallery, :on => :collection
-      end 
+      end
       member do
         match :print, :via => [:get, :post]
         # get :print
@@ -142,27 +142,27 @@ ShelterExchangeApp::Application.routes.draw do
         get :auto_complete
       end
     end
-  
+
     # Shelters
     #----------------------------------------------------------------------------
-    resources :shelters, :only => [:index, :edit, :update] 
-  
+    resources :shelters, :only => [:index, :edit, :update]
+
     # Wish Lists
-    #----------------------------------------------------------------------------    
+    #----------------------------------------------------------------------------
     resources :wish_lists, :only => [:edit, :update]
-  
+
     # Capacities
     #----------------------------------------------------------------------------
     resources :capacities
-  
+
     # Account Settings
     #----------------------------------------------------------------------------
-    resources :settings, :only => [:index] 
+    resources :settings, :only => [:index]
     match '/settings/:tab' => 'settings#index', :as => :setting
-    
+
     resources :token_authentications, :only => [:create, :destroy]
-    
-    
+
+
     # Exports
     #----------------------------------------------------------------------------
     resources :exports do
@@ -170,18 +170,18 @@ ShelterExchangeApp::Application.routes.draw do
         get :all_animals
       end
     end
-    
+
     # Integrations
     #----------------------------------------------------------------------------
     resources :integrations
-    
-    
+
+
     # Announcements
     #----------------------------------------------------------------------------
     resources :announcements do
       post :hide, :on => :collection
     end
-  
+
     # Users
     #----------------------------------------------------------------------------
     resources :users, :except => [:show, :new, :create] do
@@ -192,18 +192,18 @@ ShelterExchangeApp::Application.routes.draw do
       end
       post :invite, :on => :collection
     end
-  
-  
-    devise_for :users, 
+
+
+    devise_for :users,
                :path => "",
-               :path_names => { :sign_in => "login", :sign_out => "logout", 
-                                :confirmation => "confirmation", :invitation => "invitation" } 
-  
-  
+               :path_names => { :sign_in => "login", :sign_out => "logout",
+                                :confirmation => "confirmation", :invitation => "invitation" }
+
+
     # Dashboard
     #----------------------------------------------------------------------------
     match "/dashboard", :to => 'dashboard#index'
-  
+
     # ROOT
     #----------------------------------------------------------------------------
     root :to => redirect("/dashboard")
@@ -213,19 +213,19 @@ ShelterExchangeApp::Application.routes.draw do
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  
-  
+
+
 
   # Admin - Routes for manage.shelterexchange.org
   #----------------------------------------------------------------------------
   constraints(ShelterExchange::Subdomains::Admin) do
-        
+
     namespace :admin do
-      
+
       # Admin :: Dashboard
       #----------------------------------------------------------------------------
       resources :dashboard, :only => [:index]
-            
+
       # Admin :: Shelters
       #----------------------------------------------------------------------------
       resources :shelters, :only => [:index, :show, :edit, :update] do
@@ -235,31 +235,36 @@ ShelterExchangeApp::Application.routes.draw do
       # Admin :: Integrations
       #----------------------------------------------------------------------------
       resources :integrations, :only => [:index]
-      
+
       # Admin :: Users
       #----------------------------------------------------------------------------
       resources :users, :only => [:index] do
         get :live_search, :on => :collection
       end
-      
+
       # Admin :: Announcements
       #----------------------------------------------------------------------------
       resources :announcements, :except => [:new]
-      
+
       # Admin :: Accounts
       #----------------------------------------------------------------------------
       resources :accounts, :only => [:edit, :update]
-      
+
       # Admin :: Export
       #----------------------------------------------------------------------------
       resources :exports, :only => [:index] do
-        get :all_emails, :on => :collection
+        collection do
+          get :all_emails
+          get :petfinder_emails
+          get :adopt_a_pet_emails
+          get :all_integrations_emails
+        end
       end
-      
+
       # Admin :: Reports
       #----------------------------------------------------------------------------
       resources :reports do
-        collection do 
+        collection do
           get :status_by_month_year
           get :type_by_month_year
           get :adoptions_monthly_total
@@ -274,7 +279,7 @@ ShelterExchangeApp::Application.routes.draw do
           get :reclaimed_monthly_total_by_type
         end
       end
-      
+
     end
 
     # Admin :: Owners :: Devise
@@ -282,45 +287,45 @@ ShelterExchangeApp::Application.routes.draw do
     match '/login' => redirect('/admin/login')
     devise_for :owners, :path => "admin", :controllers => { :sessions => "admin/owners/sessions" },
                         :path_names => { :sign_in => "login", :sign_out => "logout" }
-    
+
     # Root
     #----------------------------------------------------------------------------
     root :to => redirect("/admin/dashboard")
   end
-  
+
 
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-  
+
   # Api - Routes for api.shelterexchange.org
   #----------------------------------------------------------------------------
   constraints(ShelterExchange::Subdomains::Api) do
-        
+
     namespace :api, :path => "/" do
-      
+
       # Api :: Animals
       #----------------------------------------------------------------------------
       resources :animals, :only => [:index, :show]
       match '/:version/animals' => 'animals#index'
       match '/:version/animals/:id' => 'animals#show'
-      
+
     end
-    
+
   end
-  
-  
+
+
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
-  
-  
-  
+
+
+
   # Public - Routes for www.shelterexchange.org or shelterexchange.org
   #----------------------------------------------------------------------------
   constraints(ShelterExchange::Subdomains::Public) do
-    
+
     namespace :public, :path => "/" do
-      
+
       # Public :: Accounts
       #----------------------------------------------------------------------------
       resources :accounts, :only => [:new, :create] do  #:index, add if the error has been fixed
@@ -328,8 +333,8 @@ ShelterExchangeApp::Application.routes.draw do
       end
       get "signup" => "accounts#new", :path => :signup
       post "signup" => "accounts#create", :path => :signup
-      
-      
+
+
       # Public :: Save A Life
       #----------------------------------------------------------------------------
       resources :save_a_life do
@@ -337,7 +342,7 @@ ShelterExchangeApp::Application.routes.draw do
           get :find_animals_in_bounds
         end
       end
-      
+
       # Public :: Help A Shelter
       #----------------------------------------------------------------------------
       resources :help_a_shelter, :only => [:index, :show] do
@@ -347,31 +352,31 @@ ShelterExchangeApp::Application.routes.draw do
           get :find_animals_for_shelter
         end
       end
-            
+
       # Public :: Login
       #----------------------------------------------------------------------------
       devise_for :users, :path => "", :controllers => { :sessions => "public/users/sessions", :passwords => "public/users/passwords" },
                           :path_names => { :sign_in => "login", :sign_out => "logout" }
-                          
-      
+
+
       # Public :: Pages
       #----------------------------------------------------------------------------
       resources :pages, :only => [:index, :show, :sitemap]
       match '/sitemap.xml' => 'pages#sitemap', :format => :xml
       match '*path' => 'pages#show'
       root :to => 'pages#index'
-      
+
     end
-     
+
   end
-  
-  
+
+
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
-  
+
 
   # Catch All - If route isn't found then Four oh Four
   #----------------------------------------------------------------------------
   match "*path", :to => 'errors#routing', :status => 404
   # match "*path" => redirect("/404.html")   OLD WAY TO DO IT
-  
+
 end
