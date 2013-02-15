7c9cdaaee4a611d25f33e3e787aa519175141e58
Brian Bokor <brian.bokor@gmail.com>
2013-02-14 23:17:56 -0800

    updated airbrake to correct values

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,3 @@
 Airbrake.configure do |config|
   config.api_key = '038a86657017a6f8e031c8a9311e461b'
-  config.host = 'bokor.airbrake.io'
-  config.development_environments = ["development", "test"]
 end
