67ac31c835b7c6109c198e9d386dfd48e9259482
Brian Bokor <brian.bokor@gmail.com>
2013-03-16 12:37:03 -0700

    updated activity to be a class method and not instantiate

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
   respond_to :html, :js
-  
+
   def index
     @transfers = @current_shelter.transfers.includes(:shelter, :requestor_shelter, :animal).active.all
-    @latest_activity = Activity.new(@current_shelter).recent(10)
+    @latest_activity = Activity.recent(@current_shelter)
   end
-  
+
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,21 +1,14 @@
 class Activity
-  def initialize(shelter)
-    @current_shelter ||= shelter
-    @total_recent = 20
-  end
 
-  # TODO :
-  #   1) the limit passed in here doesn't do anything.  Maybe update the recent activity methods to
-  #      use the limit.  Also look at the @total_recent (think it is the max on the page and document
-  #      remove tap and make the sorting more obvious.
-  #   2) Remove it as a class.  It is not needed and can just be a class method.
-  #   3) Add self. to the front of recent.
-  def self.recent(limit=10)
+  def self.recent(shelter)
+    limit        = 10
+    total_recent = 20
+
     [].tap do |collection|
-      collection << @current_shelter.alerts.recent_activity(10)
-      collection << @current_shelter.tasks.recent_activity(10)
-      collection << @current_shelter.animals.recent_activity(10)
-    end.flatten.sort_by(&:updated_at).reverse.slice(0, @total_recent)
+      collection << shelter.alerts.recent_activity(limit)
+      collection << shelter.tasks.recent_activity(limit)
+      collection << shelter.animals.recent_activity(limit)
+    end.flatten.sort_by(&:updated_at).reverse.slice(0, total_recent)
   end
 
 end
