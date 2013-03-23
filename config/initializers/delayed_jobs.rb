c994c3b6749cc38ee0efdb5f452fae7f1cd4fd72
Brian Bokor <brian.bokor@gmail.com>
2013-03-23 16:49:36 -0700

    bumped settings in delayed jobs

--- config/initializers/delayed_jobs.rb
+++ config/initializers/delayed_jobs.rb
@@ -1,6 +1,7 @@
 # config/initializers/delayed_job_config.rb
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 3
-Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.read_ahead = 10
+Delayed::Worker.max_run_time = 10.minutes
 Delayed::Worker.delay_jobs = !Rails.env.test?
 
