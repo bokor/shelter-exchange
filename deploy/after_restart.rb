ac7c6f0ef15605114774c3647c1a02e32b2f5777
Brian Bokor <brian.bokor@gmail.com>
2013-03-23 16:58:06 -0700

    updated util server name

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,11 +1,11 @@
 # Restart Delayed Job
 if environment == "production"
-  on_utilities("shelter_exchange_util"){ sudo "monit -g dj_shelter_exchange_app restart all" }
+  on_utilities("background_jobs"){ sudo "monit -g dj_shelter_exchange_app restart all" }
 elsif ["staging"].include?(environment)
   sudo "monit -g dj_shelter_exchange_app restart all"
 end
 
 # Ping Search Engines - Updated Sitemap.xml
 # if environment == "production"
-#   on_utilities("shelter_exchange_util"){ run "cd #{current_path} && bundle exec rake sitemap:ping" }
+#   on_utilities("background_jobs"){ run "cd #{current_path} && bundle exec rake sitemap:ping" }
 # end
