41b3a898f2851bda546c91dec6b5c65c75acd94b
Brian Bokor <brian.bokor@gmail.com>
2013-04-04 20:28:58 -0700

    fixed an issue with the maps

--- public/javascripts/public/help_a_shelter.js
+++ public/javascripts/public/help_a_shelter.js
@@ -3,53 +3,52 @@
  * Copyright (c) 2011 Designwaves, LLC. All rights reserved.
  * ------------------------------------------------------------------------ */
 var HelpAShelter = {
-	initialize: function(latitude, longitude, overlay, marker){
+	initialize: function(latitude, longitude, overlay){
 		mapOverlay = overlay;
-		logo = marker;
-		lat = latitude;
-		lng = longitude;
-		
+		lat        = latitude;
+		lng        = longitude;
+
 		Maps.createMap();
-		
+
 		$("#form_city_zipcode_search").bind("submit", function(e){
 			e.preventDefault();
 			Maps.geocodeAddress();
 		});
-		
+
 		$("#search_by_city_zipcode").bind("click",function(e, first){
 			e.preventDefault();
 		});
-		
+
 		HelpAShelter.searchByCityZipCode();
 
-	},	
+	},
 	filterInitialize: function(){
 		HelpAShelter.bindFilters();
 		Maps.breedAutoComplete(function(){HelpAShelter.findAnimalsForShelter()});
 		HelpAShelter.findAnimalsForShelter();
-		
+
 	},
 	searchByCityZipCode: function() {
-		
+
 		// Add Google Map Listener
 		idleListener = google.maps.event.addListener(map, 'idle', function(e){
 			mapCenter = map.getCenter();
 			HelpAShelter.findSheltersInBounds();
 		});
-		
+
 		resizeListener = google.maps.event.addDomListener(window, 'resize', function() {
 		  map.setCenter(mapCenter);
 		});
-		
+
 		// Set up forms
 		Maps.addressAutoComplete();
   	},
 	findSheltersInBounds: function(){
 		var bounds = map.getBounds();
-		
+
 		$("#filters_sw").val(bounds.getSouthWest().toUrlValue());
 		$("#filters_ne").val(bounds.getNorthEast().toUrlValue());
-		
+
 		$.ajax({
 			url: "/help_a_shelter/find_shelters_in_bounds",
 			type: "get",
@@ -75,9 +74,9 @@ var HelpAShelter = {
 	},
 	bindFilters: function(){
 		$("#form_filters").bind("keypress", function(e){
-			return !(window.event && window.event.keyCode == 13); 
+			return !(window.event && window.event.keyCode == 13);
 		});
-		
+
 		$("#filters_animal_type").bind("change", function(e){
 			e.preventDefault();
 			$("#filters_breed").val("");
@@ -90,16 +89,16 @@ var HelpAShelter = {
 			}
 			HelpAShelter.findAnimalsForShelter();
 		});
-		
+
 		$("#filters_sex").bind("change", function(e){
 			e.preventDefault();
 			HelpAShelter.findAnimalsForShelter();
 		});
-		
+
 		$("#filters_euthanasia_only, #filters_special_needs_only").bind($.browser.msie? "propertychange" : "change", function(e) {
 		  	e.preventDefault();
 			HelpAShelter.findAnimalsForShelter();
 		});
-		
+
 	}
 };
--- public/javascripts/public/save_a_life.js
+++ public/javascripts/public/save_a_life.js
@@ -3,39 +3,38 @@
  * Copyright (c) 2011 Designwaves, LLC. All rights reserved.
  * ------------------------------------------------------------------------ */
 var SaveALife = {
-	initialize: function(latitude, longitude, overlay, marker){
+	initialize: function(latitude, longitude, overlay){
 		mapOverlay = overlay;
-		logo = marker;
-		lat = latitude;
-		lng = longitude;
-		
+		lat        = latitude;
+		lng        = longitude;
+
 		Maps.createMap();
-		
+
 		$("#map_auto_scroll").jScroll({top: 50, speed : "slow"});
-		
+
 		$("#form_city_zipcode_search").bind("submit", function(e){
 			e.preventDefault();
 			Maps.geocodeAddress();
 		});
-		
+
 		$("#search_by_city_zipcode").bind("click",function(e, first){
 			e.preventDefault();
 		});
 
 		SaveALife.searchByCityZipCode();
-	},	
+	},
 	searchByCityZipCode: function() {
-		
+
 		// Add Google Map Listener
 		idleListener = google.maps.event.addListener(map, 'idle', function(e){
 			mapCenter = map.getCenter();
 			SaveALife.findAnimalsInBounds();
 		});
-		
+
 		resizeListener = google.maps.event.addDomListener(window, 'resize', function() {
 		  	map.setCenter(mapCenter);
 		});
-		
+
 		// Set up forms
 		SaveALife.bindFilters();
 		Maps.breedAutoComplete(function(){SaveALife.findAnimalsInBounds()});
@@ -43,10 +42,10 @@ var SaveALife = {
   	},
 	findAnimalsInBounds: function(){
 		var bounds = map.getBounds();
-		
+
 		$("#filters_sw").val(bounds.getSouthWest().toUrlValue());
 		$("#filters_ne").val(bounds.getNorthEast().toUrlValue());
-		
+
 		$.ajax({
 			url: "/save_a_life/find_animals_in_bounds",
 			type: "get",
@@ -54,24 +53,24 @@ var SaveALife = {
 			data: $("#form_filters").serialize(),
 			success: function( data ) {
 				// Update Google Analytics if production
-				if (typeof(_gaq) != "undefined") {  _gaq.push(['_trackPageview', "/save_a_life/search/"+$("#city_zipcode").val()]); } 
+				if (typeof(_gaq) != "undefined") {  _gaq.push(['_trackPageview', "/save_a_life/search/"+$("#city_zipcode").val()]); }
 			}
 		});
 	},
 	bindFilters: function(){
 		$("#form_filters").bind("keypress", function(e){
-			return !(window.event && window.event.keyCode == 13); 
+			return !(window.event && window.event.keyCode == 13);
 		});
-		
+
 		$("#form_city_zipcode_search").bind("submit", function(e){
 			e.preventDefault();
 			Maps.geocodeAddress();
 		});
-		
+
 		$("#form_shelter_name_search").bind("submit", function(e){
 			e.preventDefault();
 		});
-		
+
 		$("#filters_animal_type").bind("change", function(e){
 			e.preventDefault();
 			$("#filters_breed").val("");
@@ -84,12 +83,12 @@ var SaveALife = {
 			}
 			SaveALife.findAnimalsInBounds();
 		});
-		
+
 		$("#filters_sex").bind("change", function(e){
 			e.preventDefault();
 			SaveALife.findAnimalsInBounds();
 		});
-		
+
 		$("#filters_euthanasia_only, #filters_special_needs_only").bind($.browser.msie? "propertychange" : "change", function(e) {
 		  	e.preventDefault();
 			SaveALife.findAnimalsInBounds();
