cd381c644f4f209feb64e88789bea9981aae7184
Brian Bokor <brian.bokor@gmail.com>
2013-04-26 17:50:01 -0700

    Fixed deploy code for EY

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -5,5 +5,12 @@ sudo "ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime"
 config_file = (environment == "production" ? "config/assets.yml" : "config/assets_#{environment}.yml")
 on_app_master(){ run "cd #{current_path} && bundle exec jammit-s3 --config #{config_file}" }
 
+# Start Delayed Job
+if environment == "production"
+  on_utilities("background_jobs"){ sudo "monit start all -g dj_shelter_exchange_app" }
+elsif ["staging"].include?(environment)
+  sudo "monit start all -g dj_shelter_exchange_app"
+end
+
 # Clear Temp Cache
 # run "cd #{current_path} && RAILS_ENV=#{environment} bundle exec rake cache:clear"
