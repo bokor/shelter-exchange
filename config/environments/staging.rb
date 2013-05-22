38f700bc139de9d678dc638d6ea33ead26235796
Brian Bokor <brian.bokor@gmail.com>
2013-05-22 01:18:39 -0700

    takes too long to precompile

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -42,26 +42,27 @@ ShelterExchangeApp::Application.configure do
   config.assets.digest = true
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ["*.js", "*.css"]
+  #    This works but takes twice as long to precompile
+  #    config.assets.precompile += ["*.js", "*.css"]
   # Javascripts
-  #config.assets.precompile += %W(
-    #admin.js
-    #api.js
-    #app_print.js
-    #login.js
-    #public.js
-    #selectivizr.js
-    #html5.js
-  #)
+  config.assets.precompile += %W(
+    admin.js
+    api.js
+    app_print.js
+    login.js
+    public.js
+    selectivizr.js
+    html5.js
+  )
   # Stylesheets
-  #config.assets.precompile += %W(
-    #admin.css
-    #api.css
-    #app_print.css
-    #login.css
-    #public.css
-    #public_popup.css
-  #)
+  config.assets.precompile += %W(
+    admin.css
+    api.css
+    app_print.css
+    login.css
+    public.css
+    public_popup.css
+  )
 
   # Use a different cache store in production
   # config.cache_store = :dalli_store, "127.0.0.1:11211"
