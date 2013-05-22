ad7cb4d3700215acf967261044f390a3e532cb2f
Brian Bokor <brian.bokor@gmail.com>
2013-05-22 00:15:40 -0700

    clean up deploy issues

--- lib/shelter_exchange/jobs.rb
+++ lib/shelter_exchange/jobs.rb
@@ -1,3 +1,4 @@
 require 'shelter_exchange/jobs/facebook_linter_job'
 require 'shelter_exchange/jobs/map_overlay_job'
-require 'shelter_exchange/jobs/tweet_job'
+require 'shelter_exchange/jobs/adopt_a_pet_job'
+require 'shelter_exchange/jobs/petfinder_job'
