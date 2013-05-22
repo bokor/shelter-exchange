b99b4eac113a9284594f88310242abe8b29d5acf
Brian Bokor <brian.bokor@gmail.com>
2013-05-21 22:43:20 -0700

    application config cleanup

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,11 @@ require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 require 'rails/all'
+#require "rails/test_unit/railtie"
+#require "active_record/railtie"
+#require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "active_resource/railtie"
 #require "sprockets/railtie"
 
 if defined?(Bundler)
@@ -37,6 +42,11 @@ module ShelterExchangeApp
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # Added to load the international Files by folder name - streamlined into subfolders
+    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    #config.i18n.default_locale = :es
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password, :password_confirmation]
 
@@ -52,7 +62,7 @@ module ShelterExchangeApp
     config.assets.prefix = "/assets"
 
     config.assets.paths << Rails.root.join("app/assets/documents")
-    config.assets.paths << Rails.root.join("app/assets/html")
+    # config.assets.paths << Rails.root.join("app/assets/html")
 
     # Devise - Allows Devise to use the UrlHelper file for Subdomain links in the emails.
     config.to_prepare do
@@ -61,29 +71,19 @@ module ShelterExchangeApp
       end
     end
 
+    # Enable escaping HTML in JSON.
+    # config.active_support.escape_html_entities_in_json = true
+
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    # config.active_record.whitelist_attributes = true
   end
 end
-
-                        # Enable escaping HTML in JSON.
-                        # config.active_support.escape_html_entities_in_json = true
-
-                        # Use SQL instead of Active Record's schema dumper when creating the database.
-                        # This is necessary if your schema can't be completely dumped by the schema dumper,
-                        # like if you have constraints or database-specific column types
-                        # config.active_record.schema_format = :sql
-
-                        # Enforce whitelist mode for mass assignment.
-                        # This will create an empty whitelist of attributes available for mass-assignment for all models
-                        # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
-                        # parameters by using an attr_accessible or attr_protected declaration.
-                        # config.active_record.whitelist_attributes = true
-
-
-# Not needed right now
-#--------------------------
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # Added to load the international Files by folder name - streamlined into subfolders
-    #config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-    #config.i18n.default_locale = :es
-
 
