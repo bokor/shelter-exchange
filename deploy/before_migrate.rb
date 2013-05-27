05362cb33262741a566f53752e6a2c4b47ee7662
Brian Bokor <brian.bokor@gmail.com>
2013-05-27 01:57:37 -0700

    removed stop from deploy hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,6 @@
-# Stop Delayed Job
-if environment == "production"
-  on_utilities("background_jobs"){ sudo "monit stop all -g dj_shelter_exchange_app" }
-elsif ["staging"].include?(environment)
-  sudo "monit stop all -g dj_shelter_exchange_app"
-end
+# Stop Delayed Job - Doesn't seem to work
+#if environment == "production"
+  #on_utilities("background_jobs"){ sudo "monit stop all -g dj_shelter_exchange_app" }
+#elsif ["staging"].include?(environment)
+  #sudo "monit stop all -g dj_shelter_exchange_app"
+#end
