984d34ee47d60f50e44ec8af27aa364a722b47b9
Brian Bokor <brian.bokor@gmail.com>
2013-05-27 01:39:16 -0700

    reverted deploy script

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,6 +1,6 @@
 # Stop Delayed Job
-#if environment == "production"
-  #on_utilities("background_jobs"){ run "sudo monit stop all -g dj_shelter_exchange_app" }
-#elsif ["staging"].include?(environment)
-  #run "sudo monit stop all -g dj_shelter_exchange_app"
-#end
+if environment == "production"
+  on_utilities("background_jobs"){ run "sudo monit stop all -g dj_shelter_exchange_app" }
+elsif ["staging"].include?(environment)
+  run "sudo monit stop all -g dj_shelter_exchange_app"
+end
