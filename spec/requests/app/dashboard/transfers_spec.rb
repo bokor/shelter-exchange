1a4cc8b89b7f1db2e3a28db4ce56ffbbca662d50
Brian Bokor <brian.bokor@gmail.com>
2013-05-27 10:14:21 -0700

    added google analytic support for application

--- spec/requests/app/dashboard/transfers_spec.rb
+++ spec/requests/app/dashboard/transfers_spec.rb
@@ -6,3 +6,14 @@ describe "Transfers: For Dashboard Index Page", :js => :true do
     @account, @user, @shelter = login
   end
 end
+# Dashboard sidebar
+  #<% unless @transfers.blank? %>
+    #<div class="transfer_requests">
+      #<h2>Transfer Requests</h2>
+      #<div id="transfers">
+        #<%= render "transfers/transfer_list", :transfers => @transfers %>
+      #</div>
+    #</div>
+	#<% end %>
+#<% end %>
+
