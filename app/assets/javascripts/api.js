14362a9e75eccbbc22870817e7e121f8d4f4734e
Brian Bokor <brian.bokor@gmail.com>
2013-05-27 23:45:59 -0700

    api had a pagination issue with javascript

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -2,6 +2,4 @@
 //= require modernizr
 //= require date
 //= require jquery_ujs
-//
-//= require_tree ./shared/
 
--- app/controllers/api/animals_controller.rb
+++ app/controllers/api/animals_controller.rb
@@ -1,7 +1,7 @@
 class Api::AnimalsController < Api::ApplicationController
   respond_to :html, :json
   before_filter :configure, :api_version
-    
+
   def index
     unless request.format.html?
       @animals = @current_shelter.animals.api_lookup(@types, @statuses).all
@@ -9,49 +9,47 @@ class Api::AnimalsController < Api::ApplicationController
     else
       @animals = @current_shelter.animals.api_lookup(@types, @statuses).paginate(:page => params[:page]).all
     end
-    
+
     respond_to do |format|
       format.html
       format.json{ render :json => @animal_presenter.to_json, :callback => params[:callback] }
     end
   end
-  
-  
+
   def show
     @animal = @current_shelter.animals.find(params[:id])
     @animal_presenter = "Api::#{api_version.upcase}::AnimalPresenter".constantize.new(@animal)
-    
+
     respond_to do |format|
       format.json{ render :json => @animal_presenter.to_json, :callback => params[:callback] }
     end
   end
-  
-  
-  
-    
+
+  #----------------------------------------------------------------------------
   private
-   
-    def configure
-      @types =  params.has_key?(:types) ? params[:types].split(",").collect{|x| x.to_i } : []
-      @statuses = params.has_key?(:statuses) ? params[:statuses].split(",").collect{|x| x.to_i } : []
 
-      raise ShelterExchange::Errors::ApiIncorrectTypeStatus if @statuses.include?(AnimalStatus::STATUSES[:deceased]) or @statuses.include?(AnimalStatus::STATUSES[:euthanized])
-    end
-    
-    def api_version
-      params[:version] || CURRENT_API_VERSION
-    end
-    
-    rescue_from Exception do |exception|
-      respond_with_error({ :error => "Unexpected error has occurred.  Please review the API documentation to make sure everything is correct." })
-    end
+  def configure
+    @types =  params.has_key?(:types) ? params[:types].split(",").collect{|x| x.to_i } : []
+    @statuses = params.has_key?(:statuses) ? params[:statuses].split(",").collect{|x| x.to_i } : []
 
-    rescue_from NameError do |exception|
-      respond_with_error({ :error => "Incorrect API Version.  Please review the API documentation to make sure everything is correct." })
-    end
+    raise ShelterExchange::Errors::ApiIncorrectTypeStatus if @statuses.include?(AnimalStatus::STATUSES[:deceased]) or @statuses.include?(AnimalStatus::STATUSES[:euthanized])
+  end
 
-    rescue_from ShelterExchange::Errors::ApiIncorrectTypeStatus do |exception|
-      respond_with_error({ :error => "Contains Animal Types or Statuses that are not allowed.  Please refer to the help documentation to resolve this issue." })
-    end
-  
+  def api_version
+    params[:version] || CURRENT_API_VERSION
+  end
+
+  rescue_from Exception do |exception|
+    respond_with_error({ :error => "Unexpected error has occurred.  Please review the API documentation to make sure everything is correct." })
+  end
+
+  rescue_from NameError do |exception|
+    respond_with_error({ :error => "Incorrect API Version.  Please review the API documentation to make sure everything is correct." })
+  end
+
+  rescue_from ShelterExchange::Errors::ApiIncorrectTypeStatus do |exception|
+    respond_with_error({ :error => "Contains Animal Types or Statuses that are not allowed.  Please refer to the help documentation to resolve this issue." })
+  end
+
 end
+
