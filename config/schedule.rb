1047d19777c32a5f122c6d372072ea167a31162b
Brian Bokor <brian.bokor@gmail.com>
2013-05-29 00:11:31 -0700

    updated schedule for cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,10 @@
-if Rails.env.production?
-  # All Integrations every day at 2:00am
-  every 1.day, :at => '2:00 am' do
-    rake "integrations:start"
-  end
+# All Integrations every day at 2:00am
+every 1.day, :at => '2:00 am' do
+  rake "integrations:start"
+end
 
-  # Ping Search Engines
-  every 1.day, :at => '5:00 am' do
-    rake "sitemap:ping"
-  end
+# Ping Search Engines
+every 1.day, :at => '5:00 am' do
+  rake "sitemap:ping"
 end
 
