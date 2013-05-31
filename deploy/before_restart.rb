5bf815aadae55f8176d8cbae9d5b8daa3891109f
Brian Bokor <brian.bokor@gmail.com>
2013-05-31 14:28:07 -0700

    updated deployment script

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,12 +1,23 @@
+app_name = "shelter_exchange_app"
+puts "APPLICATION NAME :: #{app}"
 # Set Current TimeZone
 sudo "ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime"
 
 # Restart Delayed Job
+restart_delayed_jobs = "monit restart all -g dj_#{app_name}"
 if environment == "production"
-  on_utilities("background_jobs"){ sudo "monit restart all -g dj_shelter_exchange_app" }
-elsif ["staging"].include?(environment)
-  sudo "monit restart all -g dj_shelter_exchange_app"
+  on_utilities("background_jobs"){ sudo(restart_delayed_jobs) }
+elsif environment == "staging"
+  sudo(restart_delayed_jobs)
+end
+
+# Update Crontab from Whenever
+if environment == "production"
+  on_utilities("background_jobs"){
+    run "cd #{current_path} && bundle exec whenever --update-crontab '#{app_name}_#{environment}'"
+  }
 end
 
 # Clear Temp Cache
 # run "cd #{current_path} && RAILS_ENV=#{environment} bundle exec rake cache:clear"
+
