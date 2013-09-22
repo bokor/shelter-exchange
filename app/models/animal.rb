b4f9b2bb2fa9525cbd128ee3c7e50a64907e5637
Brian Bokor <brian.bokor@gmail.com>
2013-09-22 02:14:42 -0700

    Remove animal concerns and fixed validation issues

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,8 +1,7 @@
 class Animal < ActiveRecord::Base
-  include Statusable, Typeable, Uploadable # Concerns
-  include Reportable, Searchable, Mappable, Apiable # Animal Concerns
+  include Statusable, Typeable, Uploadable
 
-  default_scope :order => 'animals.updated_at DESC'
+  default_scope :order => "animals.updated_at DESC"
 
   # Constants
   #----------------------------------------------------------------------------
@@ -15,11 +14,9 @@ class Animal < ActiveRecord::Base
     :XL => "X-Large"
   }.freeze
 
-
-
   # Callbacks
   #----------------------------------------------------------------------------
-  before_save :change_status_date!
+  before_save :change_status_date!, :clean_fields
   after_validation :update_breed_names # FIXME: Remove later when we store the breed ids
   after_save :create_status_history!
 
@@ -47,44 +44,165 @@ class Animal < ActiveRecord::Base
   has_many :photos, :as => :attachable, :dependent => :destroy
   accepts_nested_attributes_for :photos, :limit => Photo::MAX_TOTAL,
                                          :allow_destroy => true,
-                                         :reject_if => Proc.new { |a| a['image'].blank? if a['image_cache'].blank? }
+                                         :reject_if => Proc.new { |a| a["image"].blank? if a["image_cache"].blank? }
 
   # Validations
   #----------------------------------------------------------------------------
   validates :name, :presence => true
-  validates :animal_type_id, :presence => { :message => 'needs to be selected' }
-  validates :animal_status_id, :presence => { :message => 'needs to be selected' }
-  validates :primary_breed, :breed => true
-  validates :secondary_breed, :breed => true, :allow_blank => true
+  validates :animal_type_id, :presence => { :message => "needs to be selected" }
+  validates :animal_status_id, :presence => { :message => "needs to be selected" }
+  validates :primary_breed, :breed_format => true
+  validates :secondary_breed, :breed_format => true
   validates :sex, :presence => true
-  validates :age, :presence => { :if => Proc.new { |a| AnimalStatus::ACTIVE.include?(a.animal_status_id) },
-                                 :message => "needs to be selected" }
-  validates :size, :presence => { :if => Proc.new { |a| AnimalStatus::ACTIVE.include?(a.animal_status_id) },
-                                  :message => "needs to be selected" }
-  validates :microchip, :uniqueness => { :allow_blank => true, :scope => :shelter_id, :message => "already exists in your shelter. Please return to the main Animal page and search by this microchip number to locate this record." }
+  validates :age, :presence => {
+    :if => Proc.new { |a| AnimalStatus::ACTIVE.include?(a.animal_status_id) },
+    :message => "needs to be selected"
+  }
+  validates :size, :presence => {
+    :if => Proc.new { |a| AnimalStatus::ACTIVE.include?(a.animal_status_id) },
+    :message => "needs to be selected"
+  }
+  validates :microchip, :uniqueness => {
+    :allow_blank => true,
+    :scope => :shelter_id,
+    :message => "already exists in your shelter. Please return to the main Animal page and search by this microchip number to locate this record."
+  }
   validates :special_needs, :presence => { :if => :special_needs? }
   validates :video_url, :video_url_format => true, :allow_blank => true
   validates :date_of_birth, :date_format => true
   validates :arrival_date, :date_format => true
   validates :euthanasia_date, :date_format => true
 
-
-  # Scopes
+  # General Scopes
   #----------------------------------------------------------------------------
-  # scope :with_shelter, includes(:shelter)
-  # scope :with_photos, includes(:photos)
-  # scope :with_animal_type, includes(:animal_type)
-  # scope :with_animal_status, includes(:animal_status)
   scope :latest, lambda { |status, limit| includes(:shelter, :photos).send(status).order("status_change_date DESC").limit(limit) }
+  scope :auto_complete, lambda { |q| includes(:animal_type, :animal_status).where("name LIKE ?", "%#{q}%") }
+  scope :search, lambda { |q|
+    includes(:animal_type, :animal_status, :photos).
+    where("animals.id LIKE ? OR animals.name LIKE ? OR animals.description LIKE ? OR
+           animals.microchip LIKE ? OR animals.color LIKE ? OR animals.weight LIKE ? OR
+           animals.primary_breed LIKE ? OR animals.secondary_breed LIKE ?",
+           "%#{q}%", "%#{q}%", "%#{q}%", "%#{q}%", "%#{q}%", "%#{q}%", "%#{q}%", "%#{q}%")
+  }
 
-
-  # Scopes - Dashboard - Recent Activity
+  # Dashboard - Recent Activity
   #----------------------------------------------------------------------------
   def self.recent_activity(limit=10)
     includes(:animal_type, :animal_status).reorder("animals.updated_at DESC").limit(limit)
   end
   #----------------------------------------------------------------------------
 
+  # API
+  #----------------------------------------------------------------------------
+  def self.api_lookup(types, statuses)
+    scope = self.scoped
+    scope = scope.includes(:animal_type, :animal_status, :photos)
+    scope = (statuses.blank? ? scope.available : scope.where(:animal_status_id => statuses))
+    scope = scope.where(:animal_type_id => types) unless types.blank?
+    scope = scope.reorder("ISNULL(animals.euthanasia_date), animals.euthanasia_date ASC")
+    scope
+  end
+  #----------------------------------------------------------------------------
+
+  # Maps and Community
+  #----------------------------------------------------------------------------
+  def self.community_animals(shelter_ids, filters={})
+    scope = self.scoped
+    scope = scope.includes(:animal_type, :animal_status, :shelter, :photos)
+    scope = scope.where(:shelter_id => shelter_ids)
+    # Fitler Euthanasia
+    scope = scope.joins(:shelter).where("shelters.is_kill_shelter = ?", true).where("animals.euthanasia_date < ?", Date.today + 2.weeks) unless filters[:euthanasia_only].blank? or !filters[:euthanasia_only]
+    # Filter Special Needs
+    scope = scope.where(:has_special_needs => true) unless filters[:special_needs_only].blank? or !filters[:special_needs_only]
+    # Filter Animal Type
+    scope = scope.where(:animal_type_id => filters[:animal_type]) unless filters[:animal_type].blank?
+    # Filter Breed
+    scope = scope.where("animals.primary_breed = ? OR animals.secondary_breed = ?", filters[:breed], breed) unless filters[:breed].blank?
+    # Filter Sex
+    scope = scope.where(:sex => filters[:sex].downcase) unless filters[:sex].blank?
+    # Filter Animal Status
+    scope = scope.where(:animal_status_id => filters[:animal_status]) unless filters[:animal_status].blank?
+    scope = scope.active unless filters[:animal_status].present?
+
+    if shelter_ids.is_a?(Array) && shelter_ids.any?
+      scope.reorder("FIELD(shelter_id, #{shelter_ids.join(',')}), ISNULL(animals.euthanasia_date), animals.euthanasia_date ASC")
+    else
+      scope.reorder("ISNULL(animals.euthanasia_date), animals.euthanasia_date ASC")
+    end
+  end
+  #----------------------------------------------------------------------------
+
+
+  # Searching
+  #----------------------------------------------------------------------------
+  def self.search_by_name(q)
+    scope = self.scoped
+    scope = scope.includes(:animal_type, :animal_status, :photos)
+    if q.is_numeric?
+      scope = scope.where(:"animals.id" => q)
+    else
+      scope = scope.where("animals.name LIKE ?", "%#{q}%")
+    end
+
+    scope
+  end
+
+  def self.filter_by_type_status(type, status)
+    scope = self.scoped
+    scope = scope.includes(:animal_type, :animal_status, :photos)
+    scope = scope.where(:animal_type_id => type) unless type.blank?
+    unless status.blank?
+      scope = (status == "active" or status == "non_active") ? scope.send(status) : scope.where(:animal_status_id => status)
+    end
+
+    scope
+  end
+  #----------------------------------------------------------------------------
+
+  # Reporting
+  #----------------------------------------------------------------------------
+  scope :count_by_type, select("count(*) count, animal_types.name").joins(:animal_type).group(:animal_type_id)
+  scope :count_by_status, select("count(*) count, animal_statuses.name").joins(:animal_status).group(:animal_status_id)
+  scope :current_month, where(:status_change_date => Date.today.beginning_of_month..Date.today.end_of_month)
+  scope :year_to_date, where(:status_change_date => Date.today.beginning_of_year..Date.today.end_of_year)
+
+  def self.type_by_month_year(month, year, shelter_id=nil, state=nil)
+    start_date = (month.blank? or year.blank?) ? Date.today : Date.civil(year.to_i, month.to_i, 01)
+    range = start_date.beginning_of_month..start_date.end_of_month
+    status_histories = StatusHistory.where(:shelter_id => shelter_id || {}).by_month(range)
+
+    scope = self.scoped
+    scope = scope.select("count(*) count, animal_types.name")
+    scope = scope.joins(:status_histories, :animal_type)
+    unless state.blank?
+      scope = scope.joins(:shelter)
+      scope = scope.where(:shelters => { :state => state })
+    end
+    scope = scope.where(:status_histories => {:id => status_histories})
+    scope = scope.where(:animal_status_id => AnimalStatus::ACTIVE)
+    scope = scope.group(:animal_type_id)
+    scope
+  end
+
+  def self.intake_totals_by_month(year, with_type=false)
+    start_date = year.blank? ? Date.today.beginning_of_year : Date.parse("#{year}0101").beginning_of_year
+    end_date = year.blank? ? Date.today.end_of_year : Date.parse("#{year}0101").end_of_year
+    scope = self.scoped
+
+    if with_type
+      scope = scope.select("animal_types.name as type").joins(:animal_type).group(:animal_type_id)
+    else
+      scope = scope.select("'Total' as type")
+    end
+
+    start_date.month.upto(end_date.month) do |month|
+      scope = scope.select("COUNT(CASE WHEN animals.created_at BETWEEN '#{start_date.beginning_of_month}' AND '#{start_date.end_of_month}' THEN 1 END) AS #{Date::MONTHNAMES[month].downcase}")
+      start_date = start_date.next_month
+    end
+    scope = scope.reorder(nil).limit(nil)
+    scope
+  end
+  #----------------------------------------------------------------------------
 
   # Instance Methods
   #----------------------------------------------------------------------------
@@ -145,6 +263,43 @@ class Animal < ActiveRecord::Base
 
   def create_status_history!
     StatusHistory.create_with(self.shelter_id, self.id, self.animal_status_id, @status_history_reason) if self.new_record? or self.animal_status_id_changed? or self.shelter_id_changed?
+  end
+
+  def clean_fields
+    clean_description
+    clean_secondary_breed
+    clean_special_needs
+  end
+
+  def clean_description
+    # Remove Microsoft Extra Smart Formatting
+    unless self.description.blank?
+      self.description.strip!
+      self.description.gsub!(/[\u201C\u201D\u201E\u201F\u2033\u2036]/, '"')
+      self.description.gsub!(/[\u2018\u2019\u201A\u201B\u2032\u2035\uFFFD]/, "'")
+      self.description.gsub!(/[\u2013\u2014]/, "-")
+      self.description.gsub!(/\u02C6/, "^")
+      self.description.gsub!(/\u2039/, "<")
+      self.description.gsub!(/\u203A/, ">")
+      self.description.gsub!(/\u2013/, "-")
+      self.description.gsub!(/\u2014/, "--")
+      self.description.gsub!(/\u2026/, "...")
+      self.description.gsub!(/\u00A9/, "&copy;")
+      self.description.gsub!(/\u00AE/, "&reg;")
+      self.description.gsub!(/\u2122/, "&trade;")
+      self.description.gsub!(/\u00BC/, "&frac14;")
+      self.description.gsub!(/\u00BD/, "&frac12;")
+      self.description.gsub!(/\u00BE/, "&frac34;")
+      self.description.gsub!(/[\u02DC\u00A0]/, " ")
+    end
+  end
+
+  def clean_secondary_breed
+    self.secondary_breed = nil unless self.mix_breed?
+  end
+
+  def clean_special_needs
+    self.special_needs = nil unless self.special_needs?
   end
 end
 
