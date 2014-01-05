7d4a2d943da78a59b85001b1f816f82c41cd20e2
Brian Bokor <brian.bokor@gmail.com>
2014-01-04 22:36:49 -0800

    removed crappy zip libraries

--- lib/shelter_exchange/jobs/map_overlay_job.rb
+++ lib/shelter_exchange/jobs/map_overlay_job.rb
@@ -6,12 +6,20 @@ module ShelterExchange
         # write cache key(overlay-kmz-date) with date
         FOG_BUCKET.files.create(
           :key => "maps/overlay.kmz",
-          :body => Zippy.new("overlay.kml" => MapsController.new.overlay).data,
+          :body => zip_archive_data,
           :public => true,
           :content_type => Mime::KMZ,
           :cache_control => "max-age=315576000",
           :expires => 1.year.from_now.httpdate
         )
+      end
+
+      private
+
+      def zip_archive_data
+        Zip::Archive.open_buffer(Zip::CREATE) do |archive|
+          archive.add_buffer("overlay.kml", MapsController.new.overlay);
+        end
       end
 
     end
