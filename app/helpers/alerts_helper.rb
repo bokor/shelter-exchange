ec0e827e76bb0cc6c25cdb2264d42c9c54383c66
Brian Bokor <brian.bokor@gmail.com>
2014-02-14 22:42:09 -0800

    added new test and updated formatting

--- app/helpers/alerts_helper.rb
+++ app/helpers/alerts_helper.rb
@@ -1,11 +1,10 @@
 module AlertsHelper
 
   def show_alertable_link(alert)
-	  if controller_name == "alerts" and alert.alertable
-	    link = link_to alert.alertable.name, polymorphic_path(alert.alertable)
-	    return ('<span class="alertable_link">(' + link + ')</span>').html_safe
+    if controller_name == "alerts" && alert.alertable
+      link = link_to alert.alertable.name, polymorphic_path(alert.alertable)
+      "<span class='alertable_link'>(#{link})</span>".html_safe
     end
   end
-
 end
 
