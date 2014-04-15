4fb7ea77d8b7e3f916c21f9f31a2dac5441f033f
Brian Bokor <brian.bokor@gmail.com>
2014-04-15 00:35:46 -0700

    added new test

---
+++ spec/helpers/maps_helper_spec.rb
@@ -0,0 +1,41 @@
+require "spec_helper"
+
+describe MapsHelper, "#map_shelter_icon" do
+  # def map_shelter_icon
+  #   if hostname = Rails.application.config.action_controller.asset_host
+  #     "http://#{hostname}/assets/logo_xsmall.png"
+  #   else
+  #     image_path("logo_xsmall.png")
+  #   end
+  # end
+  # SAMPLE
+  # it "returns text when approved" do
+  #   transfer = Transfer.gen :status => "approved"
+
+  #   expect(
+  #     helper.status_action_name(transfer)
+  #   ).to eq("Approve")
+  # end
+end
+
+describe MapsHelper, "#shelter_info_window" do
+  # def shelter_info_window(shelter)
+  #   output =  "<h2><a href='#{Rails.application.routes.url_helpers.public_help_a_shelter_url(shelter, :subdomain => 'www')}'>#{shelter.name}</a></h2>"
+  #   output << '<ul>'
+  #   output << '<li>' << shelter.street << '</li>'
+  #   output << '<li>' << shelter.street_2 << '</li>' unless shelter.street_2.blank?
+  #   output << '<li>' << shelter.city << ', ' << shelter.state << ' ' << shelter.zip_code << '</li>'
+  #   output << '<li style="padding-bottom: 10px;">' << number_to_phone(shelter.phone, :delimiter => "-") << '</li>' unless shelter.phone.blank?
+  #   unless shelter.email.blank? and shelter.website.blank?
+  #     output << '<li>'
+  #     output << '<strong style="padding:0 5px 0 0;"><a href="mailto:' << shelter.email << '">Email us</a></strong>' unless shelter.email.blank?
+  #     output << '|' if shelter.email.present? and shelter.website.present?
+  #     output << '<strong style="padding:0 0 0 5px;"><a href="' << shelter.website << '" target="_blank">Visit Website</a>' unless shelter.website.blank?
+  #     output << '</li>'
+  #   end
+  #   output << '</ul>'
+  #   output << '<div style="width:100%; text-align:center; margin: 0 auto;"><img src="' << shelter.logo.url(:thumb) << '" alt="" /></div>' if shelter.logo?
+  #   output
+  # end
+end
+
