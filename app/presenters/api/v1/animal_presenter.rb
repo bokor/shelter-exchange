3b6136cb8f153a99fd84cc6487339323bef44ceb
Brian Bokor <brian.bokor@gmail.com>
2014-05-31 19:03:20 -0700

    removed helpers not needed for new impl

--- app/presenters/api/v1/animal_presenter.rb
+++ app/presenters/api/v1/animal_presenter.rb
@@ -18,7 +18,7 @@ class Api::V1::AnimalPresenter < Presenter
         :sterilized => @animal.sterilized? ? true : false,
         :age_range => @animal.age.humanize,
         :date_of_birth => @animal.date_of_birth,
-        :date_of_birth_in_text => help.humanize_dob(@animal.date_of_birth),
+        :date_of_birth_in_text => @animal.date_of_birth.present? ? time_ago_in_words(@animal.date_of_birth) : "",
         :size => Animal::SIZES[@animal.size.to_sym],
         :color => @animal.color,
         :microchip => @animal.microchip,
