3aba41da52febfd2f29411363a28154ad7d618e8
Brian Bokor <brian.bokor@gmail.com>
2014-06-22 22:55:19 -0700

    added integration column for web access

--- app/controllers/admin/integrations_controller.rb
+++ app/controllers/admin/integrations_controller.rb
@@ -2,15 +2,18 @@ class Admin::IntegrationsController < Admin::ApplicationController
   respond_to :html, :js
 
   def index
-    integrations       = Integration.joins(:shelter).includes(:shelter).order('shelters.name ASC').all
+    @petfinder_count = Integration::Petfinder.count
+    @adopt_a_pet_count = Integration::AdoptAPet.count
+    @access_token_count = Shelter.where("shelters.access_token IS NOT NULL").count
 
-    @petfinder_count   = integrations.select{|i| i.to_sym == :petfinder }.count
-    @adopt_a_pet_count = integrations.select{|i| i.to_sym == :adopt_a_pet }.count
+    @api_access = Hash.new([])
+    Shelter.includes(:integrations).find_each do |shelter|
+      @api_access[shelter] += [:access_token] unless shelter.access_token.blank?
 
-    @integrations_hash = Hash.new([])
-
-    integrations.each do |integration|
-      @integrations_hash[integration.shelter] += [integration.to_sym]
+      shelter.integrations.each do |integration|
+        @api_access[shelter] += [integration.to_sym]
+      end
     end
   end
 end
+
