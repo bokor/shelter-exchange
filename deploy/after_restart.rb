6b0d73066ba0f9cc0b54ea93ece660c061d5067e
Brian Bokor <brian.bokor@gmail.com>
2014-10-26 00:13:44 -0700

    updated deploy scripts

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,8 +1,8 @@
 # Restart Delayed Job
 restart_delayed_jobs = "monit -g dj_#{config.app} restart all"
-case config.environment
-when "production"
-  on_utilities("background_jobs"){ sudo restart_delayed_jobs }
-when "staging"
+# case config.environment
+# when "production"
+#   on_utilities("background_jobs"){ sudo restart_delayed_jobs }
+# when "staging"
   sudo restart_delayed_jobs
-end
+# end
--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -2,11 +2,11 @@
 sudo "ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime"
 
 # Update Crontab from Whenever
-if config.environment == "production"
-  on_utilities("background_jobs"){
+# if config.environment == "production"
+#   on_utilities("background_jobs"){
     run "cd #{config.current_path} && bundle exec whenever --update-crontab '#{config.app}_#{config.environment}'"
-  }
-end
+  # }
+# end
 
 # Clear Temp Cache
 # run "cd #{current_path} && RAILS_ENV=#{environment} bundle exec rake cache:clear"
