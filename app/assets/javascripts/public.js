e98803b6435509ad2d5c482ce527f8823e39f80a
Brian Bokor <brian.bokor@gmail.com>
2017-06-25 20:38:17 -0700

    removed newsletter signup form

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -37,36 +37,3 @@ $(function() {
 	});
 });
 
-
-/* Newsletter Form
---------------------------------------------------------------------------- */
-$(function() {
-	$('#newsletter_form input[type=text]').bind('focus', function(e){
-		$(this).val('');
-		$(this).removeClass("error");
-	}).bind('blur', function(e){
-		var blurStr = $(this).val();
-		if (blurStr == ''){
-			$(this).val("Enter email address");
-		}
-	});
-
-	$("#newsletter_form").submit(function(e){
-		e.preventDefault();
-
-		var email = $(this).find("input[type=text]").val();
-		var valid = validateEmail(email);
-		if (valid){
-			$.post("https://shelterexchange.wufoo.com/forms/x7x1x7/#public", $(this).serialize());
-			$(this).find("input[type=text]").val('');
-			$(this).hide();
-			$(this).after('<span class="thank_you">Thank you!</span>');
-		} else {
-			$(this).find("input[type=text]").val("Enter a valid e-mail address");
-			$(this).find("input[type=text]").addClass("error");
-		}
-	});
-});
-
-var validateEmail = function(value) { return /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i.test(value); };
-
